506eae927d70b1f9cd97f5af8e5880c9ab718c5b
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-11 11:39:37 +0100

    ST-70 Add Elements class and its dependencies plus small change in tslint.json

---
+++ src/core/classes/Elements.ts
@@ -0,0 +1,75 @@
+import { iframesEndpoints, styleForIframe } from '../imports/iframe';
+
+const { cardNumber, cardVerificationCode, expirationDate } = iframesEndpoints;
+
+export class Elements {
+  private _name: string;
+  private _iframeSrc: string;
+  private _style: object;
+
+  get style(): object {
+    return this._style;
+  }
+
+  set style(value: object) {
+    this._style = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get iframeSrc(): string {
+    return this._iframeSrc;
+  }
+
+  set iframeSrc(value: string) {
+    this._iframeSrc = value;
+  }
+
+  constructor() {
+    this._name = '';
+    this._style = {
+      color: '#fff',
+      fontWeight: 600,
+      fontFamily: 'Lato, sans-serif',
+      fontSize: '16px',
+      fontSmoothing: 'antialiased',
+    };
+  }
+
+  static getComponentAdress(name: string) {
+    if (name === 'cardNumber') {
+      return cardNumber;
+    } else if (name === 'secureCode') {
+      return cardVerificationCode;
+    } else if (name === 'expirationDate') {
+      return expirationDate;
+    }
+  }
+
+  create(elementName: string, attributes: any) {
+    this._name = elementName;
+    this._style = attributes;
+    this._iframeSrc = Elements.getComponentAdress(elementName);
+  }
+
+  mount(fieldId: string) {
+    let iframe = document.createElement('iframe');
+    let iframeStyles: any = Object.keys(styleForIframe).map(item => {
+      return `style[${item}]=${styleForIframe[item]}`;
+    });
+    iframeStyles = iframeStyles.join('&');
+    iframe.setAttribute(
+      'src',
+      `${this._iframeSrc}?${JSON.stringify(iframeStyles)}`
+    );
+    iframe.setAttribute('id', fieldId);
+    Object.assign(iframe.style, styleForIframe);
+    return iframe;
+  }
+}
---
+++ src/core/imports/iframe.ts
@@ -0,0 +1,18 @@
+const iframesEndpoints = {
+  cardNumber: '//localhost:8081/cardNumber.html',
+  cardVerificationCode: '//localhost:8081/cardVerificationCode.html',
+  expirationDate: '//localhost:8081/expirationDate.html',
+};
+
+const styleForIframe = {
+  border: 'none',
+  margin: '0',
+  padding: '0',
+  width: '1px',
+  minWidth: '100%',
+  overflow: 'hidden',
+  display: 'block',
+  height: '120px',
+};
+
+export { iframesEndpoints, styleForIframe };
