774f43e93e3286a251fbab9d94615ce0fa9c3ace
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-12 12:42:20 +0100

    ST-70 function for getting styles from URL

--- src/core/validation/iframe/iframe.ts
+++ src/core/validation/iframe/iframe.ts
@@ -2,19 +2,7 @@ export const getStylesFromUrl = (url: string) => {
   let urlAndParams = url.split('?');
   urlAndParams.shift();
   let params = urlAndParams[0].split('&');
-  params.map(item => {
-    return item.replace();
-  });
-  // let arry = [];
-  // let arry1 = [];
-  // @ts-ignore
-  // for (var key of urlParams.keys()) {
-  //   if (key.startsWith('style')) {
-  //     arry.push(urlParams.get(key));
-  //     let newKey = key.trim();
-  //     newKey = key.replace('style[', '');
-  //     newKey = newKey.replace(']', '');
-  //     arry1.push(newKey);
-  //   }
-  // }
+  params = params.map(item => item.replace('style[', ''));
+  params = params.map(item => item.replace(']=', ':'));
+  return params;
 };
