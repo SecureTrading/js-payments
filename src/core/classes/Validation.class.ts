0dbd416b904ee87ec2eb0dd14727f3f8bb8362c8
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-13 13:02:42 +0100

    ST-50 library structure - move images to base64 and attach to classes

--- src/core/classes/Validation.class.ts
+++ src/core/classes/Validation.class.ts
@@ -1,4 +1,19 @@
 import { creditCardRegexes, creditCardBlocks } from './../imports/regex';
+import { cardsLogos } from '../imports/images';
+
+const {
+  amex,
+  astropaycard,
+  chip,
+  diners,
+  discover,
+  jcb,
+  laser,
+  maestro,
+  mastercard,
+  piba,
+  visa,
+} = cardsLogos;
 
 class Validation {
   constructor() {}
@@ -29,33 +44,27 @@ class Validation {
 
   getCardLogo(brand: string) {
     if (brand === 'amex') {
-      return 'https://s3-eu-central-1.amazonaws.com/centaur-wp/designweek/prod/content/uploads/2018/04/10121846/am_amex_06.jpg';
-    } else if (brand === 'dankort') {
-      return 'https://vignette.wikia.nocookie.net/logopedia/images/b/b4/Dankort_logo_1983-2001.png/revision/latest?cb=20171104083621';
+      return amex;
+    } else if (brand === 'astropaycard') {
+      return astropaycard;
     } else if (brand === 'diners') {
-      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2aDEyCorzJ53G5u56L2IZw_TuYRi3e1U70hqrxrrmCWrpuLUb';
+      return diners;
     } else if (brand === 'discover') {
-      return 'https://jleconsultants.com/wp-content/uploads/2018/01/Discover-Logo-370x370.jpeg';
-    } else if (brand === 'instapayment') {
-      return 'https://mark.trademarkia.com/logo-images/bitpay-llc/instapay-85386032.jpg';
+      return discover;
     } else if (brand === 'jcb') {
-      return 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/JCB_logo.svg/300px-JCB_logo.svg.png';
-    } else if (brand === 'jcb15') {
-      return 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/JCB_logo.svg/300px-JCB_logo.svg.png';
-    } else if (brand === 'mastercard') {
-      return `http://logok.org/wp-content/uploads/2014/03/Mastercard-logo-2016-logotype.png`;
+      return jcb;
+    } else if (brand === 'laser') {
+      return laser;
     } else if (brand === 'maestro') {
-      return 'https://brand.mastercard.com/content/dam/mccom/brandcenter/other-marks/ms_vrt_87_2x.png';
-    } else if (brand === 'mir') {
-      return 'http://onlinereg.ru/ICINS2018/cards2.png';
-    } else if (brand === 'uatp') {
-      return 'https://uatp.com/wp-content/uploads/2016/08/website_UATPLogo.png';
-    } else if (brand === 'unionPay') {
-      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5i6ga6VK-wA7dgb3itjsnRRuqabySYEc8Tg4gZ13NkJ3CWr7U';
+      return maestro;
+    } else if (brand === 'mastercard') {
+      return mastercard;
+    } else if (brand === 'piba') {
+      return piba;
     } else if (brand === 'visa') {
-      return 'https://resources.mynewsdesk.com/image/upload/t_next_gen_article_large_480/ojf8ed4taaxccncp6pcp.jpg';
+      return visa;
     } else {
-      return 'https://image.flaticon.com/icons/svg/138/138286.svg';
+      return chip;
     }
   }
 
