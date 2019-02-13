9c55e6d1d19e3f05aba886cc2807b5cb37929bc0
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-13 15:20:34 +0100

    ST-50 is payment available validation method

--- src/core/classes/Payment.class.ts
+++ src/core/classes/Payment.class.ts
@@ -1,5 +1,19 @@
 class Payment {
-  constructor() {}
+  private _payments: string[];
+  private _availablePayments: string[];
+
+  constructor(payments: string[]) {
+    this._payments = payments;
+  }
+
+  isPaymentAvailable() {
+    this._payments.map(item => {
+      if (!this._availablePayments.includes(item)) {
+        console.log(`${item} payment type is not available !`);
+      }
+    });
+    // will return wheter indicated payment is available or not
+  }
 }
 
 export default Payment;
