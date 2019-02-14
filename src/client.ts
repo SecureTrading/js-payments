2b9ee92ac7277969388330a277fb896ce1152d9c
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-14 09:47:35 +0100

    ST-50 method for creating elements

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,36 @@ import './components/creditCardNumber/style.scss';
 import Payment from './core/classes/Payment.class';
 import ST from './core/classes/ST.class';
 
+const elementStyles = {
+  style: {
+    base: {
+      color: '#fff',
+      fontFamily: 'Quicksand, Open Sans, Segoe UI, sans-serif',
+      fontSize: '16px',
+      fontSmoothing: 'antialiased',
+      fontWeight: 600,
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a',
+    },
+  },
+  invalid: {
+    color: '#fff',
+    ':focus': {
+      color: '#FA755A',
+    },
+    '::placeholder': {
+      color: '#FFCCA5',
+    },
+  },
+};
+const elementClasses = {
+  focus: 'focus',
+  empty: 'empty',
+  invalid: 'invalid',
+};
+
 const createElement = () => {
   const element = document.createElement('form');
   element.setAttribute('id', 'st-form');
@@ -18,61 +48,28 @@ const createElement = () => {
   const expirationDate = new Element();
 
   cardNumber.create('cardNumber', {
-    style: {
-      base: {
-        color: '#fff',
-        fontFamily: 'Quicksand, Open Sans, Segoe UI, sans-serif',
-        fontSize: '16px',
-        fontSmoothing: 'antialiased',
-        fontWeight: 600,
-      },
-      invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a',
-      },
-    },
+    style: elementStyles,
+    classes: elementClasses,
   });
+  const cardNumberMounted = cardNumber.mount('st-card-number');
 
   securityCode.create('securityCode', {
-    style: {
-      base: {
-        color: '#fff',
-        fontFamily: 'Quicksand, Open Sans, Segoe UI, sans-serif',
-        fontSize: '16px',
-        fontSmoothing: 'antialiased',
-        fontWeight: 600,
-      },
-      invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a',
-      },
-    },
+    style: elementStyles,
+    classes: elementClasses,
   });
+  const securityCodeMounted = securityCode.mount('st-security-code');
 
   expirationDate.create('expirationDate', {
-    style: {
-      base: {
-        color: '#fff',
-        fontFamily: 'Quicksand, Open Sans, Segoe UI, sans-serif',
-        fontSize: '16px',
-        fontSmoothing: 'antialiased',
-        fontWeight: 600,
-      },
-      invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a',
-      },
-    },
+    style: elementStyles,
+    classes: elementClasses,
   });
-
-  const cardNumberMounted = cardNumber.mount('st-card-number');
-  const securityCodeMounted = securityCode.mount('st-security-code');
   const expirationDateMounted = expirationDate.mount('st-expiration-date');
+
   cardNumber.register(
     [cardNumberMounted, securityCodeMounted, expirationDateMounted],
     'st-form'
   );
 
   const st = new ST('thisissupersecretidforaclient12344321');
-  const payment = new Payment(['ApplePay', 'GooglePay']);
+  const payment = new Payment(['ApplePay', 'GooglePay'], ['133456', '546565']);
 })();
--- src/core/classes/Payment.class.ts
+++ src/core/classes/Payment.class.ts
@@ -1,20 +1,27 @@
 class Payment {
   private _payments: string[];
+  private _codes: string[];
   private _availablePayments: string[];
 
-  constructor(payments: string[]) {
+  constructor(payments: string[], codes: string[]) {
     this._payments = payments;
+    this._codes = codes;
     this._availablePayments = ['ApplePay', 'VisaCheckout'];
-    this.isPaymentAvailable();
+    this.arePaymentsAvailable();
   }
 
-  isPaymentAvailable() {
+  arePaymentsAvailable() {
     this._payments.map(item => {
       if (!this._availablePayments.includes(item)) {
         alert(`${item} payment type is not available !`);
       }
     });
     // will return wheter indicated payment is available or not
+  }
+
+  setAvailablePayments() {
+    // load script / payment library url,
+    // load initial config with  user key supported
   }
 }
 
