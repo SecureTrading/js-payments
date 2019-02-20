3cbcc08fedc040492ee0e0b6540bf6a1f153bd97
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-20 15:59:18 +0100

    ST-59 security code  validation

--- components/securityCode/src/index.ts
+++ components/securityCode/src/index.ts
@@ -1,4 +1,5 @@
 import './style.scss';
+import SecurityCode from '../../../src/core/classes/validation/SecurityCode.class';
 import { inputListener } from '../../../src/core/listeners/input';
 
 inputListener(
@@ -7,3 +8,6 @@ inputListener(
   'security-code',
   'security-code'
 );
+
+const sc = new SecurityCode();
+sc.isCharNumber(document.getElementById('security-code'));
--- src/core/classes/validation/SecurityCode.class.ts
+++ src/core/classes/validation/SecurityCode.class.ts
@@ -1,8 +1,43 @@
 import Validation from './Validation.class';
 
 class SecurityCode extends Validation {
+  static AMEX_CARD_NAME = 'AMEX';
+  static AMEX_SECURITY_CODE_LENGTH = 4;
+  static NONAMEX_SECURITY_CODE_LENGTH = 3;
+  static KEYCODES_DIGIT = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+  static KEYCODES_NUMPAD = [96, 97, 98, 99, 100, 101, 102, 103, 104, 105];
+
   constructor() {
     super();
+  }
+
+  /**
+   *
+   * @param length Length of indicated code
+   * @param cardType
+   */
+  isLengthCorrect(length: number, cardType: string) {
+    if (cardType === SecurityCode.AMEX_CARD_NAME) {
+      if (length === SecurityCode.AMEX_SECURITY_CODE_LENGTH) {
+        return true;
+      }
+    } else {
+      if (length === SecurityCode.NONAMEX_SECURITY_CODE_LENGTH) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  isCharNumber(field: HTMLElement) {
+    field.addEventListener('keypress', event => {
+      if (
+        !SecurityCode.KEYCODES_DIGIT.includes(event.keyCode) ||
+        !SecurityCode.KEYCODES_NUMPAD.includes(event.keyCode)
+      ) {
+        event.preventDefault();
+      }
+    });
   }
 }
 
