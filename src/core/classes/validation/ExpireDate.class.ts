8eb9bb60590f412ed0a8b892ef5b983fb5874bf8
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-20 14:14:25 +0100

    ST-59 expiration date validation

--- src/core/classes/validation/ExpireDate.class.ts
+++ src/core/classes/validation/ExpireDate.class.ts
@@ -1,8 +1,37 @@
 import Validation from './Validation.class';
 
 class ExpireDate extends Validation {
+  static KEYBOARD_CODES_FOR_DIGITS = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+  static MONTH_KEYBOARD_CODE_FIRST_CHAR = [48, 49];
+  static DATE_MAX_LENGTH = 5;
+  static DATE_SLASH_PLACE = 2;
+  static DATE_EMPTY = 0;
+
   constructor() {
     super();
+  }
+
+  dateInputMask(element: HTMLInputElement) {
+    element.addEventListener('keyup', (event: any) => {
+      let inputValueLength = element.value.length;
+      if (inputValueLength < ExpireDate.DATE_MAX_LENGTH) {
+        if (!ExpireDate.KEYBOARD_CODES_FOR_DIGITS.includes(event.keyCode)) {
+          event.preventDefault();
+        }
+        if (inputValueLength === ExpireDate.DATE_EMPTY) {
+          if (
+            ExpireDate.MONTH_KEYBOARD_CODE_FIRST_CHAR.includes(event.keyCode)
+          ) {
+            event.preventDefault();
+          }
+        }
+        if (inputValueLength === ExpireDate.DATE_SLASH_PLACE) {
+          element.value += '/';
+        }
+      } else {
+        event.preventDefault();
+      }
+    });
   }
 }
 
