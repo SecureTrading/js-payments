2eac61167a0473a910995b475a2996d4c2fbe254
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-21 12:46:27 +0100

    ST-69 refactoring of iframe communication

--- src/core/listeners/submit.ts
+++ src/core/listeners/submit.ts
@@ -9,19 +9,23 @@ const returnErrorMessage = (errors: any) => {
     return 'Field is empty';
   }
 };
+
+/**
+ * Listens to postMessage from iframe and returns proper error
+ */
 const returnedErrorMessageListener = () => {
   window.addEventListener('message', event => {
-    if (event.origin !== appEndpoint) {
-      document.getElementById(
-        'st-form__received-message'
-      ).textContent = returnErrorMessage(event.data);
+    const { origin, data } = event;
+    const errorContainer = document.getElementById('st-form__received-message');
+    if (origin !== appEndpoint) {
+      errorContainer.textContent = returnErrorMessage(data);
     }
   });
 };
 /**
- *
- * @param id
- * @param url
+ * Listens to submit and gives iframes a sign that post has been done
+ * @param id ID of iframe
+ * @param url Target url of iframe
  */
 const submitListener = (id: string, url: string) => {
   document.addEventListener('DOMContentLoaded', () => {
