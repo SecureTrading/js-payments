4d9fc72721d73de5248352f66779471d81bdae12
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-21 12:06:08 +0100

    ST-69 refactoring of iframe communication

--- src/core/listeners/input.ts
+++ src/core/listeners/input.ts
@@ -3,6 +3,56 @@ import { applyStylesToElement } from '../helpers/dom';
 
 const { cardNumber, securityCode, expirationDate } = iframesEndpoints;
 
+/**
+ * Method returns specific endpoint for each input
+ * @param inputName
+ */
+const returnInputEndpoint = (inputName: string) => {
+  if (inputName === 'cardNumber') {
+    return `${cardNumber}/?`;
+  } else if (inputName === 'securityCode') {
+    return `${securityCode}/?`;
+  } else if (inputName === 'expirationDate') {
+    return `${expirationDate}/?`;
+  }
+};
+
+/**
+ * Method for checking validity object and sending response to parent node
+ * @param fieldId ID of field which is validated
+ */
+const inputValueListener = (fieldId: string) => {
+  let input = <HTMLInputElement>document.getElementById(fieldId);
+  window.addEventListener(
+    'message',
+    event => {
+      if (
+        event.origin !== cardNumber &&
+        event.origin !== securityCode &&
+        event.origin !== expirationDate
+      ) {
+        let isFormValid = input.validity.valid;
+        if (isFormValid) {
+          parent.postMessage(true, appEndpoint);
+        } else {
+          parent.postMessage(
+            { isEmpty: input.validity.valueMissing },
+            appEndpoint
+          );
+        }
+      }
+    },
+    true
+  );
+};
+
+/**
+ * Method aggregating applying styles and validating field
+ * @param inputName
+ * @param formId
+ * @param fieldId
+ * @param iframeId
+ */
 const inputListener = (
   inputName: string,
   formId: string,
@@ -10,33 +60,8 @@ const inputListener = (
   iframeId: string
 ) => {
   document.addEventListener('DOMContentLoaded', () => {
-    let form = document.getElementById(formId) as HTMLFormElement;
-    let input = <HTMLInputElement>document.getElementById(fieldId);
-    if (inputName === 'cardNumber') {
-      applyStylesToElement(iframeId, `${cardNumber}/?`);
-    } else if (inputName === 'securityCode') {
-      applyStylesToElement(iframeId, `${securityCode}/?`);
-    } else if (inputName === 'expirationDate') {
-      applyStylesToElement(iframeId, `${expirationDate}/?`);
-    }
-
-    window.addEventListener(
-      'message',
-      event => {
-        if (event.origin !== cardNumber) {
-          let isFormValid = Object.values(input.validity).some(item => item);
-          if (isFormValid) {
-            form.submit();
-          } else {
-            parent.postMessage(
-              { isEmpty: input.validity.valueMissing },
-              appEndpoint
-            );
-          }
-        }
-      },
-      true
-    );
+    applyStylesToElement(iframeId, returnInputEndpoint(inputName));
+    inputValueListener(fieldId);
   });
 };
 
