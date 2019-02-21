3cd00fba48e38e0b33edbd357d90a71f34986d4b
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-21 22:45:36 +0100

    ST-59 Working regexp for expiration date

--- src/core/classes/validation/ExpireDate.class.ts
+++ src/core/classes/validation/ExpireDate.class.ts
@@ -1,16 +1,45 @@
 import Validation from './Validation.class';
 
 class ExpireDate extends Validation {
+  static KEYBOARD_CODES_FOR_DIGITS = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+  static MONTH_CHARS = [48, 49];
+  static DATE_MAX_LENGTH = 5;
+  static DATE_SLASH_PLACE = 2;
+  static DATE_EMPTY = 0;
+  static EXPIRATION_DATE_REGEXP = `^(0[1-9]|1[0-2])\\/([0-9]{2})$`;
+
   constructor() {
     super();
   }
 
-  isSelectEmpty(id: string) {
-    let input = document.getElementById(id);
-    input.addEventListener('onselect', event => {
-      console.log(event);
+  /**
+   * Method for masking expiration date in format MM/YY
+   * @param element
+   */
+  dateInputMask(element: HTMLInputElement) {
+    element.addEventListener('keyup', (event: any) => {
+      let length = element.value.length;
+      if (length < ExpireDate.DATE_MAX_LENGTH) {
+        if (!ExpireDate.KEYBOARD_CODES_FOR_DIGITS.includes(event.keyCode)) {
+          event.preventDefault();
+        }
+
+        if (length === ExpireDate.DATE_EMPTY) {
+          if (ExpireDate.KEYBOARD_CODES_FOR_DIGITS.includes(event.keyCode)) {
+            event.preventDefault();
+          }
+        }
+
+        if (length === ExpireDate.DATE_SLASH_PLACE) {
+          element.value += '/';
+        }
+      } else {
+        event.preventDefault();
+      }
     });
   }
+
+  isDateValid(date: string) {}
 }
 
 export default ExpireDate;
