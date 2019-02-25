9d309ee708c449757f711935c819e0c2da1b3dad
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-02-25 12:09:16 +0100

    ST-60 some changes from prettier

--- src/core/classes/validation/Validation.class.ts
+++ src/core/classes/validation/Validation.class.ts
@@ -7,23 +7,23 @@ class Validation implements IValidation {
 
   constructor() {
     this._isValid = {
-      'creditCard': false,
-      'expireDate': false,
-      'securityCode': false,
+      creditCard: false,
+      expireDate: false,
+      securityCode: false
     };
   }
 
   getValidity(fieldName: string) {
     return this._isValid[fieldName];
-  };
+  }
 
   setValidityOfField(fieldName: string) {
     this._isValid[fieldName] = !this._isValid[fieldName];
-  };
+  }
 
   isFormValid() {
     this._isValid.some((field: boolean) => field === true);
-  };
+  }
 }
 
 export default Validation;
