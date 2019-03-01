123ba9d0ac56e97c73af2be9d5fa5037e88286e0
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-01 08:25:43 +0100

    ST-109 Security code test suite definition

--- test/core/classes/validation/SecurityCode.spec.ts
+++ test/core/classes/validation/SecurityCode.spec.ts
@@ -2,17 +2,32 @@ import SecurityCode from './../../../../src/core/classes/validation/SecurityCode
 
 // given
 describe('Class Security Code', () => {
+  const fieldId = 'security-code';
+  let securityCodeInstance = new SecurityCode(fieldId);
+
+  // when
+  beforeEach(() => {});
   // given
   describe('Method inputValidationListener', () => {
-    it('');
+    // when
+    beforeEach(() => {});
   });
 
   // given
-  describe('Method postMessageEventListener', () => {});
+  describe('Method postMessageEventListener', () => {
+    // when
+    beforeEach(() => {});
+  });
 
   // given
-  describe('Method setSecurityCodeProperties', () => {});
+  describe('Method setSecurityCodeProperties', () => {
+    // when
+    beforeEach(() => {});
+  });
 
   // given
-  describe('Method cardNumberSecurityCodeMatch', () => {});
+  describe('Method cardNumberSecurityCodeMatch', () => {
+    // when
+    beforeEach(() => {});
+  });
 });
