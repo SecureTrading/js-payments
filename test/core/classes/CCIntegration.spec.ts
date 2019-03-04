85f8c336efe0060bf8efce5d77b2263710cc7c85
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-04 11:49:38 +0100

    ST-120 UT amendments

--- test/core/classes/CCIntegration.spec.ts
+++ test/core/classes/CCIntegration.spec.ts
@@ -1,21 +1,34 @@
 import CCIntegration from './../../../src/core/classes/CCIntegration.class';
+import Cardinal from '../../../src/core/imports/cardinalLibrary';
+
+jest.mock('../../../src/core/imports/cardinalLibrary');
 
 // given
 describe('Class CCIntegration', () => {
-  let ccIntegration;
+  let ccIntegration: any;
 
-  beforeEach(() => {
-    ccIntegration = new CCIntegration();
+  beforeEach(() => {});
+  // given
+  describe('Method _setConfiguration', () => {
+    // then
+    it('should be called once', () => {});
   });
-  // given
-  describe('Method _setConfiguration', () => {});
 
   // given
-  describe('Method _onPaymentSetupComplete', () => {});
+  describe('Method _onPaymentSetupComplete', () => {
+    // then
+    it('should be called once', () => {});
+  });
 
   // given
-  describe('Method _onPaymentValidation', () => {});
+  describe('Method _onPaymentValidation', () => {
+    // then
+    it('should be called once', () => {});
+  });
 
   // given
-  describe('Method _onSetup', () => {});
+  describe('Method _onSetup', () => {
+    // then
+    it('should be called once', () => {});
+  });
 });
