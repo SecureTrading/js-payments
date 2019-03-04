3c604162b8656857d2e3ca0c0fb34c36c1579066
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-04 12:59:31 +0100

    ST-35 Visa checkout init

--- src/core/classes/VisaCheckout.class.ts
+++ src/core/classes/VisaCheckout.class.ts
@@ -1,5 +1,42 @@
 class VisaCheckout {
+  private static _visaCheckoutButtonURL: string =
+    'https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png';
+  private static _visaCheckoutButtonProps: object = {
+    alt: 'Visa Checkout',
+    class: 'v-button',
+    role: 'button'
+  };
+
   constructor() {}
+
+  private _onVisaCheckoutReady() {
+    V.init({
+      apikey: 'ZG50YOGQQJ0PWPPX8D7F21ELpFID5NF-W256C638eL5hNgsOc',
+      encryptionKey: 'cX8SKE8k5X#BO40elD0M4dJV65WZGqCi1+I#S$rZ',
+      paymentRequest: {
+        currencyCode: 'USD',
+        subtotal: '11.00'
+      }
+    });
+  }
+
+  private _onPaymentSuccess() {
+    V.on('payment.success', function(payment: object) {
+      alert(JSON.stringify(payment));
+    });
+  }
+
+  private _onPaymentCancel() {
+    V.on('payment.cancel', function(payment: object) {
+      alert(JSON.stringify(payment));
+    });
+  }
+
+  private _onPaymentError() {
+    V.on('payment.error', function(payment: object, error: object) {
+      alert(JSON.stringify(error));
+    });
+  }
 }
 
 export default VisaCheckout;
