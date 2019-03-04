75f9ffa507c4b42cafe15a981cc1c011511dd2f8
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-04 17:37:53 +0100

    ST-29 Visa implementation - test suites

--- test/core/classes/VisaCheckout.spec.ts
+++ test/core/classes/VisaCheckout.spec.ts
@@ -2,6 +2,42 @@ import VisaCheckout from '../../../src/core/classes/VisaCheckout.class';
 
 // given
 describe('Visa Checkout class', () => {
+  let instance;
   // when
-  beforeEach(() => {});
+  beforeEach(() => {
+    // instance = new VisaCheckout();
+  });
+
+  // given
+  describe('Method _createVisaButton', () => {
+    // then
+    it('should return img markup', () => {});
+
+    // then
+    it('should img markup have certain attributes', () => {});
+  });
+
+  // given
+  describe('Method __attachVisaButton', () => {
+    // then
+    it('should attach Visa Checkout button into body', () => {});
+  });
+
+  // given
+  describe('Method _onVisaCheckoutReady', () => {
+    // then
+    it('should init script on button', () => {});
+  });
+
+  // given
+  describe('Method __attachVisaButton', () => {
+    // then
+    it('should attach Visa Checkout button into body', () => {});
+  });
+
+  // given
+  describe('Method __attachVisaButton', () => {
+    // then
+    it('should attach Visa Checkout button into body', () => {});
+  });
 });
