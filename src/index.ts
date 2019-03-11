5e4790037ab7e10be070481426801fc1facac148
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-11 10:48:34 +0100

    ST-35 Merge amendments - initalizing Visa

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import ST from './core/classes/ST.class';
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1YzEyODg0NWMxMWI5MjIwZGMwNDZlOGUiLCJpYXQiOjE1NTE4Njg1NDUsImp0aSI6IjQ2LTk3MjhjZjgyMjc0NzE3NmNlZTYxY2Q0ZDBlM2YxYjE4N2Y3MWM5NTg3Nzg1MjNjNTIwNDEwMjY5NmJmMjhiNmQiLCJwYXlsb2FkIjp7Ik9yZGVyRGV0YWlscyI6eyJBbW91bnQiOjUxMTIsIkN1cnJlbmN5Q29kZSI6Ijg0MCJ9fSwiT3JnVW5pdElkIjoiNWMxMTNlOGU2ZmUzZDEyNDYwMTQxODY4In0.7_EqGOOZ1XiRMxiUgiWUUzqOzjNbX5hHpfrFxHH-F6Y',
     {},
     [
-      // {
-      //   name: 'VISA',
-      //   props: {
-      //     apikey: 'ZG50YOGQQJ0PWPPX8D7F21ELpFID5NF-W256C638eL5hNgsOc',
-      //     encryptionKey: 'cX8SKE8k5X#BO40elD0M4dJV65WZGqCi1+I#S$rZ'
-      //   }
-      // }
+      {
+        name: 'VISA',
+        props: {
+          apikey: 'ZG50YOGQQJ0PWPPX8D7F21ELpFID5NF-W256C638eL5hNgsOc',
+          encryptionKey: 'cX8SKE8k5X#BO40elD0M4dJV65WZGqCi1+I#S$rZ'
+        }
+      }
     ]
   );
 })();
