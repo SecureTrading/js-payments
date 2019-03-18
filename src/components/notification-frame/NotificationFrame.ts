0c2134cc6766e67fec45a7fad7c9730ccf6946d3
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-18 15:11:57 +0100

    ST-33 Error message component

--- src/components/notification-frame/NotificationFrame.ts
+++ src/components/notification-frame/NotificationFrame.ts
@@ -1,43 +1,43 @@
+enum messageTypes {
+  error = 'ERROR',
+  cancel = 'CANCEL',
+  sucess = 'SUCCESS'
+}
+
 /**
  * Defines component for displaying payment status messages
  */
 
-enum messageTypes {
-  error = 'ERROR',
-  sucess = 'SUCCESS',
-  cancel = 'CANCEL'
-}
-
-enum elementTypes {
-  element = 'element',
-  content = 'content'
-}
-
 class NotificationFrame {
-  private static ELEMENT_ID: string = 'st-notification-frame';
   public static ELEMENT_CLASSES = {
     error: 'notification-frame--error',
     success: 'notification-frame--success',
     cancel: 'notification-frame--cancel'
   };
+  public static _getElement = (elementId: string) => document.getElementById(elementId);
 
-  private _id: string;
+  private static ELEMENT_ID: string = 'st-notification-frame';
+
+  /**
+   * Returns proper class for every type of incoming message
+   * @param messageType
+   * @private
+   */
+  private static _getMessageClass(messageType: string) {
+    if (messageType === 'error') {
+      return NotificationFrame.ELEMENT_CLASSES.error;
+    } else if (messageType === 'success') {
+      return NotificationFrame.ELEMENT_CLASSES.success;
+    } else if (messageType === 'cancel') {
+      return NotificationFrame.ELEMENT_CLASSES.cancel;
+    }
+  }
+
   private _message: { type: messageTypes; content: string };
-  private _style: object;
-
-  set id(value: string) {
-    this._id = value;
-  }
-
-  set style(value: object) {
-    this._style = value;
-  }
 
   constructor() {
     this._errorMessageListener();
   }
-
-  public static _getElement = (elementId: string) => document.getElementById(elementId);
 
   /**
    * Inserts content of incoming text info into div
@@ -48,18 +48,8 @@ class NotificationFrame {
       NotificationFrame._getElement(NotificationFrame.ELEMENT_ID).textContent = this._message.content;
       NotificationFrame._getElement(NotificationFrame.ELEMENT_ID).setAttribute(
         'class',
-        this._getMessageClass(this._message.type)
+        NotificationFrame._getMessageClass(this._message.type)
       );
-    }
-  }
-
-  private _getMessageClass(messageType: string) {
-    if (messageType === 'error') {
-      return NotificationFrame.ELEMENT_CLASSES.error;
-    } else if (messageType === 'success') {
-      return NotificationFrame.ELEMENT_CLASSES.success;
-    } else if (messageType === 'cancel') {
-      return NotificationFrame.ELEMENT_CLASSES.cancel;
     }
   }
 
