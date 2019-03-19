43b62b83714d03004d13f489b49b78274bfc7801
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-19 18:24:59 +0100

    ST-35 CR - changes linter warnings

--- src/core/imports/apms.ts
+++ src/core/imports/apms.ts
@@ -4,8 +4,8 @@ export const apmsNames = {
 };
 
 export const VISA_CHECKOUT_URLS = {
-  PROD_BUTTON_URL: 'https://secure.checkout.visa.com/wallet-services-web/xo/button.png',
-  PROD_SDK: 'https://secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js',
   DEV_BUTTON_URL: 'https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png',
-  DEV_SDK: 'https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js'
+  DEV_SDK: 'https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js',
+  PROD_BUTTON_URL: 'https://secure.checkout.visa.com/wallet-services-web/xo/button.png',
+  PROD_SDK: 'https://secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js'
 };
