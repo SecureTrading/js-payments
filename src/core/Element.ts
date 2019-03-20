1d2bf24324ffa6944695d7bb0ab104f263f17c54
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-20 11:54:02 +0100

    ST-29 Cr changes

--- src/core/Element.ts
+++ src/core/Element.ts
@@ -13,18 +13,18 @@ class Element {
     if (name === Element.CARD_NUMBER_COMPONENT_NAME) {
       return Element.CARD_NUMBER_COMPONENT;
     } else if (name === Element.SECURITY_CODE_COMPONENT_NAME) {
-      return Element.SECURITY_CODE__COMPONENT;
+      return Element.SECURITY_CODE_COMPONENT;
     } else if (name === Element.EXPIRATION_DATE_COMPONENT_NAME) {
       return Element.EXPIRATION_DATE_COMPONENT;
     }
   }
 
-  private static CARD_NUMBER_COMPONENT_NAME: string = 'cardNumber';
-  private static SECURITY_CODE_COMPONENT_NAME: string = 'securityCode';
-  private static EXPIRATION_DATE_COMPONENT_NAME: string = 'expirationDate';
-  private static CARD_NUMBER_COMPONENT: string = '/card-number.html';
-  private static SECURITY_CODE__COMPONENT: string = '/security-code.html';
-  private static EXPIRATION_DATE_COMPONENT: string = '/expiration-date.html';
+  public static CARD_NUMBER_COMPONENT_NAME: string = 'cardNumber';
+  public static SECURITY_CODE_COMPONENT_NAME: string = 'securityCode';
+  public static EXPIRATION_DATE_COMPONENT_NAME: string = 'expirationDate';
+  public static CARD_NUMBER_COMPONENT: string = '/card-number.html';
+  public static SECURITY_CODE_COMPONENT: string = '/security-code.html';
+  public static EXPIRATION_DATE_COMPONENT: string = '/expiration-date.html';
 
   /**
    * Method for creating DOM elements
