ac016c1f75012735b10d2b6e0f0f338911088622
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-25 17:54:50 +0100

    ST-20 Animated card - test suites

---
+++ test/components/animated-card/AnimatedCard.spec.ts
@@ -0,0 +1,41 @@
+import AnimatedCard from './../../../src/components/animated-card/AnimatedCard';
+
+// given
+describe('Class AnimatedCard', () => {
+  // given
+  describe('Method ifCardExists', () => {});
+
+  // given
+  describe('Method returnThemeClass', () => {});
+
+  // given
+  describe('Method resetToDefaultTheme', () => {});
+
+  // given
+  describe('Method setThemeProperties', () => {});
+
+  // given
+  describe('Method setCardTheme', () => {});
+
+  // given
+  describe('Method setValueOnCard', () => {});
+
+  // given
+  describe('Method shouldFlipCard', () => {});
+
+  // given
+  describe('Method flipCard', () => {});
+
+  // given
+  describe('Method flipCardBack', () => {});
+
+  // given
+  describe('Method getCardData', () => {});
+});
+
+function animatedCardFixture() {
+  const instance = new AnimatedCard();
+  return {
+    instance
+  };
+}
