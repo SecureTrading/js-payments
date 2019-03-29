64c9ad37ae289fd4ebcdeb4ce203119a2d2ec016
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-03-29 14:06:52 +0100

    ST-117 CR amendments

--- webpack.test.js
+++ webpack.test.js
@@ -14,7 +14,7 @@ module.exports = merge(common, {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NormalModuleReplacementPlugin(
       /\.\.\/\.\.\/environments\/environment/,
-      '../../environments/environment.at'
+      '../../environments/environment.test'
     )
   ]
 });
