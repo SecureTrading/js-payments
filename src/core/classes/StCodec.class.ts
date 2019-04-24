cf41403f314596898158cfe2bd1a26c119ed17a7
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-04-24 13:01:16 +0200

    ST-170 Console log breakpoimt

--- src/core/classes/StCodec.class.ts
+++ src/core/classes/StCodec.class.ts
@@ -95,8 +95,8 @@ class StCodec {
       throw new Error(Language.translations.COMMUNICATION_ERROR_INVALID_RESPONSE);
     }
     const responseContent = responseData.response[0];
+    console.log(responseData);
     if (responseContent.errorcode !== '0') {
-      console.log(responseContent);
       // Should this be a custom error type which can also take a field that is at fault
       // so that errordata can be sent up to highlight the field?
       throw new Error(responseContent.errormessage);
