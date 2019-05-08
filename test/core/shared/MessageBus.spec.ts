30c6d89d5018d35f8d40d71a591bc9c909496614
James Simpson <james.simpson@securetrading.com>
2019-05-08 23:38:16 +0100

    Improved unit test coverage of MessageBus.ts message handling functions

--- test/core/shared/MessageBus.spec.ts
+++ test/core/shared/MessageBus.spec.ts
@@ -75,4 +75,94 @@ describe('MessageBus', () => {
       expect(framePostMessage).toHaveBeenCalledWith(event, 'https://localhost:8443');
     });
   });
+
+  describe('subscribeOnParent()', () => {
+    it('should add callback to subscriptions', () => {
+      let instance = new MessageBus();
+      instance.subscribeOnParent('MYEVENT', 'MYCALLBACK');
+      // @ts-ignore
+      expect(instance._subscriptions).toMatchObject({
+        MYEVENT: 'MYCALLBACK'
+      });
+
+      instance.subscribeOnParent('MYEVENT2', 'MYCALLBACK2');
+      // @ts-ignore
+      expect(instance._subscriptions).toMatchObject({
+        MYEVENT: 'MYCALLBACK',
+        MYEVENT2: 'MYCALLBACK2'
+      });
+
+      instance.subscribeOnParent('MYEVENT2', 'OVERRIDEN');
+      // @ts-ignore
+      expect(instance._subscriptions).toMatchObject({
+        MYEVENT: 'MYCALLBACK',
+        MYEVENT2: 'OVERRIDEN'
+      });
+    });
+  });
+
+  describe('registerMessageListener()', () => {
+    it('should register message listener', () => {
+      let instance = new MessageBus();
+      window.addEventListener = jest.fn();
+      // @ts-ignore
+      instance.registerMessageListener();
+      expect(window.addEventListener).toHaveBeenCalledTimes(1);
+      // @ts-ignore
+      expect(window.addEventListener).toHaveBeenCalledWith('message', instance._handleMessageEvent);
+    });
+  });
+
+  describe('_handleMessageEvent()', () => {
+    let subFunc: Function;
+    let instance: MessageBus;
+    beforeEach(() => {
+      instance = new MessageBus('https://parent.com');
+      subFunc = jest.fn();
+      // @ts-ignore
+      instance._subscriptions['MYEVENTTYPE'] = subFunc;
+    });
+
+    it('should call subscription on same origin', () => {
+      // @ts-ignore
+      instance._handleMessageEvent({
+        origin: 'https://localhost:8443',
+        data: { type: 'MYEVENTTYPE', data: 'EVENT DATA' }
+      });
+      expect(subFunc).toHaveBeenCalledTimes(1);
+      expect(subFunc).toHaveBeenCalledWith('EVENT DATA');
+    });
+
+    it('shouldnt call subscription on parent origin', () => {
+      // @ts-ignore
+      instance._handleMessageEvent({
+        origin: 'https://parent.com',
+        data: { type: 'MYEVENTTYPE', data: 'EVENT DATA' }
+      });
+      expect(subFunc).toHaveBeenCalledTimes(0);
+    });
+
+    it('shouldnt call subscription on parent origin unless public', () => {
+      // @ts-ignore
+      MessageBus.EVENTS_PUBLIC['MYEVENTTYPE'] = 'MYEVENTTYPE';
+      // @ts-ignore
+      instance._handleMessageEvent({
+        origin: 'https://parent.com',
+        data: { type: 'MYEVENTTYPE', data: 'EVENT DATA' }
+      });
+      expect(subFunc).toHaveBeenCalledTimes(1);
+      expect(subFunc).toHaveBeenCalledWith('EVENT DATA');
+    });
+
+    it('shouldnt call subscription on unknown origin even if public', () => {
+      // @ts-ignore
+      MessageBus.EVENTS_PUBLIC['MYEVENTTYPE'] = 'MYEVENTTYPE';
+      // @ts-ignore
+      instance._handleMessageEvent({
+        origin: 'https://unknown.com',
+        data: { type: 'MYEVENTTYPE', data: 'EVENT DATA' }
+      });
+      expect(subFunc).toHaveBeenCalledTimes(0);
+    });
+  });
 });
