3765c253fa76140f95d24aedfaf009c046ed2a50
James Simpson <james.simpson@securetrading.com>
2019-05-08 20:29:45 +0100

    Increase test coverage of Element.ts

--- test/core/Element.spec.ts
+++ test/core/Element.spec.ts
@@ -5,7 +5,11 @@ import Selectors from '../../src/core/shared/Selectors';
 // given
 describe('Index placeholder test', () => {
   //when
-  let element, mockObject: object, createNewElement: object, createNewStyledElement: object;
+  let element,
+    mockObject: object,
+    createNewElement: object,
+    createNewStyledElement: object,
+    createNewStyledAndParamedElement: object;
 
   beforeEach(() => {
     mockObject = {
@@ -21,32 +25,94 @@ describe('Index placeholder test', () => {
         environment.FRAME_URL
       }/card-number.html?background-color-input=AliceBlue&color-input-error=%23721c24`
     };
+    createNewStyledAndParamedElement = {
+      _name: 'cardNumber',
+      _iframeSrc: `${
+        environment.FRAME_URL
+      }/card-number.html?background-color-input=AliceBlue&color-input-error=%23721c24&locale=en_GB`
+    };
   });
 
-  //then
-  it('should have initial settings ', () => {
-    element = new Element();
-    expect(element).toMatchObject(mockObject);
+  describe('constructor', () => {
+    //then
+    it('should have initial settings ', () => {
+      element = new Element();
+      expect(element).toMatchObject(mockObject);
+    });
+
+    // then
+    it('should return proper iframe endpoints', () => {
+      expect(Element.getComponentAddress('cardNumber')).toEqual(Selectors.CARD_NUMBER_COMPONENT);
+      expect(Element.getComponentAddress('securityCode')).toEqual(Selectors.SECURITY_CODE_COMPONENT);
+      expect(Element.getComponentAddress('expirationDate')).toEqual(Selectors.EXPIRATION_DATE_COMPONENT);
+      expect(Element.getComponentAddress('notificationFrame')).toEqual(Selectors.NOTIFICATION_FRAME_COMPONENT);
+      expect(Element.getComponentAddress('controlFrame')).toEqual(Selectors.CONTROL_FRAME_COMPONENT);
+      expect(Element.getComponentAddress('animatedCard')).toEqual(Selectors.ANIMATED_CARD_COMPONENT);
+    });
+
+    // then
+    it('should create new element', () => {
+      element = new Element();
+      element.create('cardNumber', {});
+      expect(element).toMatchObject(createNewElement);
+    });
+
+    // then
+    it('should create new element with styles', () => {
+      element = new Element();
+      element.create('cardNumber', {
+        'background-color-input': 'AliceBlue',
+        'color-input-error': '#721c24'
+      });
+      expect(element).toMatchObject(createNewStyledElement);
+    });
+
+    // then
+    it('should create new element with styles and params', () => {
+      element = new Element();
+      element.create(
+        'cardNumber',
+        {
+          'background-color-input': 'AliceBlue',
+          'color-input-error': '#721c24'
+        },
+        { locale: 'en_GB' }
+      );
+      expect(element).toMatchObject(createNewStyledAndParamedElement);
+    });
   });
 
-  // then
-  it('should return proper iframe endpoints', () => {
-    expect(Element.getComponentAddress('cardNumber')).toEqual(Selectors.CARD_NUMBER_COMPONENT);
-    expect(Element.getComponentAddress('securityCode')).toEqual(Selectors.SECURITY_CODE_COMPONENT);
-    expect(Element.getComponentAddress('expirationDate')).toEqual(Selectors.EXPIRATION_DATE_COMPONENT);
+  describe('createFormElement', () => {
+    // then
+    it('should create new DOM element', () => {
+      // @ts-ignore
+      let actual = Element.createFormElement('input', 'myID');
+      expect(actual.tagName).toBe('INPUT');
+      expect(actual.id).toBe('myID');
+      expect(actual.className).toBe('myID');
+    });
   });
 
-  // then
-  it('should create new element', () => {
-    element = new Element();
-    element.create('cardNumber', {});
-    expect(element).toMatchObject(createNewElement);
+  describe('mount', () => {
+    // then
+    it('should create iframe element', () => {
+      let element = new Element();
+      element.iframeSrc = 'https://example.com';
+      let actual = element.mount('myID');
+      expect(actual.tagName).toBe('IFRAME');
+      expect(actual.id).toBe('myID');
+      expect(actual.className).toBe('myID');
+      expect(actual.getAttribute('src')).toBe('https://example.com');
+      expect(actual.getAttribute('name')).toBe('myID');
+    });
   });
 
-  // then
-  it('should create new element with styles', () => {
-    element = new Element();
-    element.create('cardNumber', { 'background-color-input': 'AliceBlue', 'color-input-error': '#721c24' });
-    expect(element).toMatchObject(createNewStyledElement);
+  describe('name', () => {
+    // then
+    it('should use set and get', () => {
+      let element = new Element();
+      element.name = 'Some name';
+      expect(element.name).toBe('Some name');
+    });
   });
 });
