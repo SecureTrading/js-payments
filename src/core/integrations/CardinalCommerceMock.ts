9236e857889621537de26eb7e9f19c8bf1ab3456
James Simpson <james.simpson@securetrading.com>
2019-05-17 18:09:51 +0100

    Change GATEWAY_URL to completely different server

--- src/core/integrations/CardinalCommerceMock.ts
+++ src/core/integrations/CardinalCommerceMock.ts
@@ -6,6 +6,10 @@ export default class CardinalCommerceMock extends CardinalCommerce {
     super(step);
   }
 
+  // TODO override _onCardinalLoad to do nothing
+  // TODO override _performBinDetection to do nothing
+  // TODO override /jwt/ in test code to return AUTH response
+
   protected _authenticateCard() {
     fetch(environment.CARDINAL_COMMERCE.MOCK.AUTHENTICATE_CARD_URL)
       .then((response: any) => {
