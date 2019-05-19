35fc9a4bb7fade30a28f21dfa7a7f6d2d76165bb
James Simpson <james.simpson@securetrading.com>
2019-05-19 05:02:21 +0100

    Change ApplePay Mock to override only ApplePaySession related functionality

--- src/core/integrations/ApplePayMock.ts
+++ src/core/integrations/ApplePayMock.ts
@@ -1,55 +1,46 @@
-import { environment } from '../../environments/environment';
 import { applePayButton } from '../imports/images';
-import { NotificationType } from '../models/NotificationEvent';
 import DomMethods from '../shared/DomMethods';
-import Language from '../shared/Language';
 import ApplePay from './ApplePay';
-
-class ApplePaySessionMock {
-  public static onvalidatemerchant: any;
-  public static onpaymentauthorized: any;
-  public static completePayment: any;
-  public static oncancel: any;
-
-  public static completeMerchantValidation() {}
-
-  public static completePaymentMethodSelection() {}
-
-  /**
-   * Retrieves Apple Pay data from test endpoint
-   * @private
-   */
-  public static begin() {
-    return fetch(environment.APPLE_PAY_URLS.MOCK_DATA_URL)
-      .then((response: any) => {
-        return response.json();
-      })
-      .then((data: any) => {
-        return data;
-      });
-  }
-}
+import ApplePaySessionMock from './ApplePaySessionMock';
 
 /**
  * Mocked version of Apple Pay setting test environment for Apple Pay automated tests.
  */
 class ApplePayMock extends ApplePay {
-  public ifApplePayIsAvailable = () => true;
+  public paymentDetails: string;
+
+  public ifApplePayIsAvailable() {
+    return true;
+  }
 
   public setApplePayVersion() {
     this.applePayVersion = 5;
   }
 
-  public checkApplePayAvailability = () => true;
+  public checkApplePayAvailability() {
+    return true;
+  }
 
-  public checkApplePayWalletCardAvailability = () => true;
+  public checkApplePayWalletCardAvailability() {
+    return new Promise((resolve, reject) => {
+      resolve(true);
+    });
+  }
 
-  public getApplePaySessionObject = () => ApplePaySessionMock;
+  public getApplePaySessionObject() {
+    return ApplePaySessionMock;
+  }
 
-  public paymentDetails: string;
+  public getPaymentStatus() {
+    return ApplePaySessionMock.STATUS_SUCCESS;
+  }
 
-  constructor(config: any, step: boolean, jwt: string) {
-    super(config, step, jwt);
+  /**
+   * Creates Apple Pay button element created from base64 image
+   * @public
+   */
+  public createApplePayButton() {
+    return DomMethods.createHtmlElement.apply(this, [{ src: applePayButton, id: 'st-apple-pay' }, 'img']);
   }
 }
 
---
+++ src/core/integrations/ApplePaySessionMock.ts
@@ -0,0 +1,34 @@
+import { environment } from '../../environments/environment';
+
+export default class ApplePaySessionMock {
+  public static STATUS_SUCCESS: any;
+  public static onvalidatemerchant: any;
+  public static onpaymentauthorized: any;
+  public static oncancel: any;
+
+  public static completeMerchantValidation = () => true;
+
+  public static completePaymentMethodSelection = () => true;
+
+  /**
+   * Retrieves Apple Pay data from test endpoint
+   * @private
+   */
+  public static begin() {
+    return fetch(environment.APPLE_PAY_URLS.MOCK_DATA_URL)
+      .then((response: any) => {
+        return response.json();
+      })
+      .then((data: any) => {
+        if (data.status === 'SUCCESS') {
+          this.STATUS_SUCCESS = data.status;
+          this.onpaymentauthorized(data);
+        } else {
+          this.oncancel(data);
+        }
+        return data;
+      });
+  }
+
+  public static completePayment = () => true;
+}
