f689c78f55cbb0c708d65751e61c1b69b2e93d31
James Simpson <james.simpson@securetrading.com>
2019-05-19 02:48:52 +0100

    Update ApplePayMock to more accurately mock apple

--- src/core/integrations/ApplePayMock.ts
+++ src/core/integrations/ApplePayMock.ts
@@ -5,15 +5,21 @@ import DomMethods from '../shared/DomMethods';
 import Language from '../shared/Language';
 import ApplePay from './ApplePay';
 
-/**
- * Mocked version of Apple Pay setting test environment for Apple Pay automated tests.
- */
-class ApplePayMock extends ApplePay {
+class ApplePaySessionMock {
+  public static onvalidatemerchant: any;
+  public static onpaymentauthorized: any;
+  public static completePayment: any;
+  public static oncancel: any;
+
+  public static completeMerchantValidation() {}
+
+  public static completePaymentMethodSelection() {}
+
   /**
    * Retrieves Apple Pay data from test endpoint
    * @private
    */
-  private static _getWalletverifyData() {
+  public static begin() {
     return fetch(environment.APPLE_PAY_URLS.MOCK_DATA_URL)
       .then((response: any) => {
         return response.json();
@@ -22,88 +28,28 @@ class ApplePayMock extends ApplePay {
         return data;
       });
   }
+}
+
+/**
+ * Mocked version of Apple Pay setting test environment for Apple Pay automated tests.
+ */
+class ApplePayMock extends ApplePay {
+  public ifApplePayIsAvailable = () => true;
+
+  public setApplePayVersion() {
+    this.applePayVersion = 5;
+  }
+
+  public checkApplePayAvailability = () => true;
+
+  public checkApplePayWalletCardAvailability = () => true;
+
+  public getApplePaySessionObject = () => ApplePaySessionMock;
 
   public paymentDetails: string;
-  private _step: boolean;
 
   constructor(config: any, step: boolean, jwt: string) {
     super(config, step, jwt);
-    this._step = step;
-    this._onMockInit();
-  }
-
-  /**
-   * Starts mocked Apple Pay flow
-   * @private
-   */
-  private _onMockInit() {
-    this._attachMockButton();
-    this._setActionOnMockedButton();
-  }
-
-  /**
-   * Attach created Apple Pay button into DOM
-   */
-  private _attachMockButton = () => DomMethods.appendChildIntoDOM(this.placement, this._createMockedButton());
-
-  /**
-   * Creates Apple Pay button element created from base64 image
-   * @private
-   */
-  private _createMockedButton = () =>
-    DomMethods.createHtmlElement.apply(this, [{ src: applePayButton, id: 'st-apple-pay-mock' }, 'img']);
-
-  /**
-   * Sets action on appended mocked Visa Checkout button
-   * @private
-   */
-  private _setActionOnMockedButton() {
-    DomMethods.addListener('st-apple-pay-mock', 'click', () => {
-      ApplePayMock._getWalletverifyData().then((data: any) => {
-        this.paymentDetails = JSON.stringify(data);
-        this._proceedFlowWithMockedData();
-      });
-    });
-  }
-
-  /**
-   * Proceeds payment flow with mocked data
-   * @private
-   */
-  private _proceedFlowWithMockedData() {
-    const paymentDetails = JSON.parse(this.paymentDetails);
-    this.merchantSession = JSON.parse(paymentDetails.walletsession);
-    this.validateMerchantRequestData.walletmerchantid = this.merchantSession.merchantIdentifier;
-    // @ts-ignore
-    if (paymentDetails.walletsession) {
-      this.mockedPaymentProcess();
-    } else {
-      this.setNotification(NotificationType.Error, Language.translations.MERCHANT_VALIDATION_FAILURE);
-    }
-  }
-
-  /**
-   * Mocked AUTH process after this.session.completePayment()
-   * @private
-   */
-  private mockedPaymentProcess() {
-    this.payment
-      .processPayment(
-        {
-          requesttypedescription: this._step ? 'CACHETOKENISE' : 'AUTH'
-        },
-        {
-          walletsource: this.validateMerchantRequestData.walletsource,
-          wallettoken: this.paymentDetails
-        },
-        DomMethods.parseMerchantForm()
-      )
-      .then(() => {
-        this.setNotification(NotificationType.Success, Language.translations.PAYMENT_SUCCESS);
-      })
-      .catch(() => {
-        this.setNotification(NotificationType.Error, Language.translations.PAYMENT_ERROR);
-      });
   }
 }
 
