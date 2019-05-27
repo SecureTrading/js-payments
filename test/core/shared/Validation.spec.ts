c6b63c0320fb24ea7a87d8be4f643b2021a9e3b4
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-05-27 17:47:20 +0200

    ST-26 Fixing problems with UT

--- test/core/shared/Validation.spec.ts
+++ test/core/shared/Validation.spec.ts
@@ -23,7 +23,7 @@ each([
   [{ valid: true, valueMissing: false }, ''],
   [{ valid: false, valueMissing: true }, Language.translations.VALIDATION_ERROR],
   [{ valid: false, patternMismatch: true }, Language.translations.VALIDATION_ERROR_PATTERN_MISMATCH],
-  [{ valid: false, tooShort: true }, Language.translations.VALIDATION_ERROR_VALUE_TOO_SHORT]
+  [{ valid: false, tooShort: true }, Language.translations.VALIDATION_ERROR]
 ]).test('Validation.getValidationMessage', (validityState, expected) => {
   const instance = new Validation();
   expect(instance.getValidationMessage(validityState)).toBe(expected);
