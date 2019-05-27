96787286e60dace0078cd5b076b7cd00b3cce944
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-05-27 16:28:11 +0200

    ST-167 babel IE support

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@
  * STJS Library.
  * This file contains logic for all ST form fields served inside IFRAME.
  */
+import '@babel/polyfill';
 import './animated-card/animated-card';
 import './card-number/card-number';
 import './expiration-date/expiration-date';
