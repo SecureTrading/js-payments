280463c4589abccacdbd9fea581d0095c90f2ebf
James Simpson <james.simpson@securetrading.com>
2019-06-06 08:20:43 +0100

    Ignore js-payments-testing and update browser name

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+  coveragePathIgnorePatterns: ['node_modules', 'js-payments-testing'],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -140,9 +140,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: ['node_modules', 'js-payments-testing'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   testRegex: '/test',
