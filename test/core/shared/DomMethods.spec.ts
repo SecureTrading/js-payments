48d12557736c02c3d7b5fb375cfa92bc5589acff
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-06-12 15:36:36 +0200

    ST-237 Add new UT for DomMethods

--- test/core/shared/DomMethods.spec.ts
+++ test/core/shared/DomMethods.spec.ts
@@ -1,3 +1,4 @@
+import SpyInstance = jest.SpyInstance;
 import DomMethods from '../../../src/core/shared/DomMethods';
 
 // given
@@ -31,34 +32,21 @@ describe('DomMethods', () => {
   describe('DomMethods.parseForm', () => {
     // then
     it('should parse st-name from form', () => {
-      const { form } = createFormFixture();
+      const { form, parseFormObject } = createFormFixture();
       const merchantData = DomMethods.parseForm(form);
-      expect(merchantData).toMatchObject({
-        stFieldName: '',
-        stFieldName2: 'some value',
-        stDuplicate: 'value2',
-        stSelectName: 'B'
-      });
+      expect(merchantData).toMatchObject(parseFormObject);
       expect(merchantData.myfield3).toBe(undefined);
     });
   });
 
   // given
   describe('DomMethods.addDataToForm', () => {
-    let data: any;
+    const { parseFormObject } = createFormFixture();
 
-    beforeAll(() => {
-      data = {
-        stFieldName: '',
-        stFieldName2: 'some value',
-        stDuplicate: 'value2',
-        stSelectName: 'B'
-      };
-    });
-
+    // then
     it('should add all fields if not provided', () => {
       const form = document.createElement('form');
-      DomMethods.addDataToForm(form, data);
+      DomMethods.addDataToForm(form, parseFormObject);
       expect(form.querySelector('[name="stFieldName"]').getAttribute('value')).toBe('');
       expect(form.querySelector('[name="stFieldName"]').tagName).toBe('INPUT');
       expect(form.querySelector('[name="stFieldName"]').getAttribute('type')).toBe('hidden');
@@ -67,9 +55,10 @@ describe('DomMethods', () => {
       expect(form.querySelector('[name="stSelectName"]').getAttribute('value')).toBe('B');
     });
 
+    // then
     it('should only add specified fields if provided', () => {
       const form = document.createElement('form');
-      DomMethods.addDataToForm(form, data, ['stFieldName', 'stFieldName2']);
+      DomMethods.addDataToForm(form, parseFormObject, ['stFieldName', 'stFieldName2']);
       expect(form.querySelector('[name="stFieldName"]').getAttribute('value')).toBe('');
       expect(form.querySelector('[name="stFieldName2"]').getAttribute('value')).toBe('some value');
       expect(form.querySelector('[name="stDuplicate"]')).toBe(null);
@@ -79,10 +68,11 @@ describe('DomMethods', () => {
 
   // given
   describe('DomMethods.parseMerchantForm', () => {
-    let spy: any;
+    let spy: SpyInstance;
+    const { html } = createFormFixture();
+    // when
     beforeEach(() => {
-      document.body.innerHTML =
-        '<form id="st-form" class="example-form"> <h1 class="example-form__title"> Secure Trading<span>AMOUNT: <strong>10.00 GBP</strong></span> </h1> <div class="example-form__section example-form__section--horizontal"> <div class="example-form__group"> <label for="example-form-name" class="example-form__label example-form__label--required">NAME</label> <input id="example-form-name" class="example-form__input" type="text" placeholder="John Doe" autocomplete="name" /> </div> <div class="example-form__group"> <label for="example-form-email" class="example-form__label example-form__label--required">E-MAIL</label> <input id="example-form-email" class="example-form__input" type="email" placeholder="test@mail.com" autocomplete="email" /> </div> <div class="example-form__group"> <label for="example-form-phone" class="example-form__label example-form__label--required">PHONE</label> <input id="example-form-phone" class="example-form__input" type="tel" placeholder="+00 000 000 000" autocomplete="tel" /> </div> </div> <div class="example-form__spacer"></div> <div class="example-form__section"> <div id="st-notification-frame" class="example-form__group"></div> <div id="st-card-number" class="example-form__group"></div> <div id="st-expiration-date" class="example-form__group"></div> <div id="st-security-code" class="example-form__group"></div> <div id="st-error-container" class="example-form__group"></div> <div class="example-form__spacer"></div> </div> <div class="example-form__section"> <div class="example-form__group"> <button type="submit" class="example-form__button">PAY</button> </div> </div> <div class="example-form__section"> <div id="st-control-frame" class="example-form__group"></div> <div id="st-visa-checkout" class="example-form__group"></div> <div id="st-apple-pay" class="example-form__group"></div> </div> <div id="st-animated-card" class="st-animated-card-wrapper"></div> </form>';
+      document.body.innerHTML = html;
       spy = jest.spyOn(DomMethods, 'parseForm');
     });
 
@@ -104,10 +94,9 @@ describe('DomMethods', () => {
   // given
   describe('DomMethods.removeChildFromDOM', () => {
     // when
+    const { htmlForParentAndChild } = createFormFixture();
     beforeEach(() => {
-      const html =
-        '<form id="st-form" class="example-form"><h1 class="example-form__title" id="some-title"> Secure Trading</h1></form>';
-      document.body.innerHTML = html;
+      document.body.innerHTML = htmlForParentAndChild;
       DomMethods.removeChildFromDOM('st-form', 'some-title');
     });
 
@@ -129,23 +118,37 @@ function addInput(form: any, name: string, value: string, stName?: string) {
 }
 
 function createFormFixture() {
+  const html =
+    '<form id="st-form" class="example-form"> <h1 class="example-form__title"> Secure Trading<span>AMOUNT: <strong>10.00 GBP</strong></span> </h1> <div class="example-form__section example-form__section--horizontal"> <div class="example-form__group"> <label for="example-form-name" class="example-form__label example-form__label--required">NAME</label> <input id="example-form-name" class="example-form__input" type="text" placeholder="John Doe" autocomplete="name" /> </div> <div class="example-form__group"> <label for="example-form-email" class="example-form__label example-form__label--required">E-MAIL</label> <input id="example-form-email" class="example-form__input" type="email" placeholder="test@mail.com" autocomplete="email" /> </div> <div class="example-form__group"> <label for="example-form-phone" class="example-form__label example-form__label--required">PHONE</label> <input id="example-form-phone" class="example-form__input" type="tel" placeholder="+00 000 000 000" autocomplete="tel" /> </div> </div> <div class="example-form__spacer"></div> <div class="example-form__section"> <div id="st-notification-frame" class="example-form__group"></div> <div id="st-card-number" class="example-form__group"></div> <div id="st-expiration-date" class="example-form__group"></div> <div id="st-security-code" class="example-form__group"></div> <div id="st-error-container" class="example-form__group"></div> <div class="example-form__spacer"></div> </div> <div class="example-form__section"> <div class="example-form__group"> <button type="submit" class="example-form__button">PAY</button> </div> </div> <div class="example-form__section"> <div id="st-control-frame" class="example-form__group"></div> <div id="st-visa-checkout" class="example-form__group"></div> <div id="st-apple-pay" class="example-form__group"></div> </div> <div id="st-animated-card" class="st-animated-card-wrapper"></div> </form>';
+  const htmlForParentAndChild =
+    '<form id="st-form" class="example-form"><h1 class="example-form__title" id="some-title"> Secure Trading</h1></form>';
+
   const form = document.createElement('form');
   addInput(form, 'myfield', '', 'stFieldName');
   addInput(form, 'myfield2', 'some value', 'stFieldName2');
   addInput(form, 'myfield3', 'ignored');
   addInput(form, 'duplicate', 'value1', 'stDuplicate');
   addInput(form, 'duplicate', 'value2', 'stDuplicate');
+
   const select = document.createElement('select');
   select.name = 'selectField';
   select.setAttribute('data-st-name', 'stSelectName');
+
   const opt1 = document.createElement('option');
   const opt2 = document.createElement('option');
+
   const scriptUrl = 'http://example.com/test.js';
+  const parseFormObject = {
+    stFieldName: '',
+    stFieldName2: 'some value',
+    stDuplicate: 'value2',
+    stSelectName: 'B'
+  };
   opt1.value = 'A';
   opt2.value = 'B';
   opt2.selected = true;
   select.appendChild(opt1);
   select.appendChild(opt2);
   form.appendChild(select);
-  return { form, scriptUrl };
+  return { form, html, htmlForParentAndChild, parseFormObject, scriptUrl };
 }
