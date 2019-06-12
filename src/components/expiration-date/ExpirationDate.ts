ae540e600e08c6e5c48c98dd55324273dc55a0f6
Grzegorz Martowski (PGS Software) <gmartowski@pgs-soft.com>
2019-06-12 10:47:10 +0200

    ST-225 Trpedocs for exp date

--- src/components/expiration-date/ExpirationDate.ts
+++ src/components/expiration-date/ExpirationDate.ts
@@ -39,6 +39,9 @@ export default class ExpirationDate extends FormField {
     return Language.translations.LABEL_EXPIRATION_DATE;
   }
 
+  /**
+   *
+   */
   public setFocusListener() {
     this._messageBus.subscribe(MessageBus.EVENTS.FOCUS_EXPIRATION_DATE, () => {
       this.format(this._inputElement.value);
@@ -46,20 +49,25 @@ export default class ExpirationDate extends FormField {
     });
   }
 
+  /**
+   * Listens to BLOCK_EXPIRATION_DATE event and toggle disable attribute and class.
+   */
   public setDisableListener() {
     this._messageBus.subscribe(MessageBus.EVENTS.BLOCK_EXPIRATION_DATE, (state: boolean) => {
       if (state) {
-        // @ts-ignore
-        this._inputElement.setAttribute(ExpirationDate.DISABLE_STATE, state);
+        this._inputElement.setAttribute(ExpirationDate.DISABLE_STATE, 'true');
         this._inputElement.classList.add(ExpirationDate.DISABLE_FIELD_CLASS);
       } else {
-        // @ts-ignore
         this._inputElement.removeAttribute(ExpirationDate.DISABLE_STATE);
         this._inputElement.classList.remove(ExpirationDate.DISABLE_FIELD_CLASS);
       }
     });
   }
 
+  /**
+   * Extends onInput method and adds masking and formatting.
+   * @param event
+   */
   protected onInput(event: Event) {
     super.onInput(event);
     this._inputElement.value = Formatter.trimNonNumericExceptSlash(this._inputElement.value);
@@ -70,15 +78,26 @@ export default class ExpirationDate extends FormField {
     this.sendState();
   }
 
+  /**
+   * Extends onFocus method.
+   * @param event
+   */
   protected onFocus(event: Event) {
     super.onFocus(event);
   }
 
+  /**
+   * Extends onBlur method and triggers sendState().
+   */
   protected onBlur() {
     super.onBlur();
     this.sendState();
   }
 
+  /**
+   * Extends onPaste method with formatting and masking.
+   * @param event
+   */
   protected onPaste(event: ClipboardEvent) {
     super.onPaste(event);
     const preparedValue = this._inputElement.value.substring(0, ExpirationDate.EXPIRATION_DATE_LENGTH);
@@ -86,6 +105,10 @@ export default class ExpirationDate extends FormField {
     this.sendState();
   }
 
+  /**
+   * Extends onKeyPress event with max length check.
+   * @param event
+   */
   protected onKeyPress(event: KeyboardEvent) {
     super.onKeyPress(event);
     if (this.isMaxLengthReached()) {
@@ -93,12 +116,22 @@ export default class ExpirationDate extends FormField {
     }
   }
 
-  protected format(data: string) {
-    this.setValue(Formatter.maskExpirationDate(data));
+  /**
+   * Formats indicated date using mask.
+   * @param date
+   */
+  protected format(date: string) {
+    this.setValue(Formatter.maskExpirationDate(date));
   }
 
+  /**
+   * Checked if indicated value length is greater that expected.
+   */
   private isMaxLengthReached = (): boolean => this._inputElement.value.length >= ExpirationDate.EXPIRATION_DATE_LENGTH;
 
+  /**
+   * Propagates expiration date change to MessageBus
+   */
   private sendState() {
     const formFieldState: IFormFieldState = this.getState();
     const messageBusEvent: IMessageBusEvent = {
