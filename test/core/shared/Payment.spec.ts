03d1d8b6083fe6233a248fd5ae39cd8d7ffff7f6
Grzegorz Martowski <gmartowski@pgs-soft.com>
2019-07-19 10:43:55 +0200

    ST-270 Payment UT improvement

--- test/core/shared/Payment.spec.ts
+++ test/core/shared/Payment.spec.ts
@@ -4,14 +4,15 @@ import StTransport from '../../../src/core/classes/StTransport.class';
 // given
 describe('Payment', () => {
   let { card, wallet, walletverify, instance } = paymentFixture();
-  // when
-  beforeAll(() => {
-    // @ts-ignore
-    instance._stTransport.sendRequest = jest.fn();
-  });
 
   // given
   describe('constructor()', () => {
+    // when
+    beforeEach(() => {
+      // @ts-ignore
+      instance._stTransport.sendRequest = jest.fn();
+    });
+
     // then
     it('should set attributes to payment instance', () => {
       // @ts-ignore
@@ -20,24 +21,13 @@ describe('Payment', () => {
   });
 
   // given
-  describe('walletVerify()', () => {
-    // then
-    it('should send WALLETVERIFY request with walletverify', () => {
-      instance.walletVerify(walletverify);
-      // @ts-ignore
-      expect(instance._stTransport.sendRequest).toHaveBeenCalledWith({
-        requesttypedescriptions: ['WALLETVERIFY'],
-        walletsource: 'APPLEPAY',
-        walletmerchantid: '123456789',
-        walletvalidationurl: 'https://example.com',
-        walletrequestdomain: 'https://example2.com'
-      });
-    });
-  });
-
-  // given
   describe('byPassInitRequest()', () => {
-    const cachetoken = 'somecachetoken';
+    const { cachetoken } = paymentFixture();
+    // when
+    beforeEach(() => {
+      // @ts-ignore
+      instance._stTransport.sendRequest = jest.fn();
+    });
     instance.byPassInitRequest(cachetoken);
     // @ts-ignore
     expect(instance._cardinalCommerceCacheToken).toEqual(cachetoken);
@@ -45,6 +35,12 @@ describe('Payment', () => {
 
   // given
   describe('processPayment()', () => {
+    // when
+    beforeEach(() => {
+      // @ts-ignore
+      instance._stTransport.sendRequest = jest.fn();
+    });
+
     // then
     it('should send AUTH request with card', () => {
       instance.processPayment(['AUTH'], card, {
@@ -57,6 +53,7 @@ describe('Payment', () => {
         merchant: 'data'
       });
     });
+
     // then
     it('should send CACHETOKENISE request', () => {
       instance.processPayment(['CACHETOKENISE'], card, {});
@@ -66,6 +63,7 @@ describe('Payment', () => {
         requesttypedescriptions: ['CACHETOKENISE']
       });
     });
+
     // then
     it('should send AUTH request with card and additional data', () => {
       instance.processPayment(
@@ -85,6 +83,7 @@ describe('Payment', () => {
         additional: 'some data'
       });
     });
+
     // then
     it('should send AUTH request with wallet', () => {
       instance.processPayment(['AUTH'], wallet, {
@@ -98,6 +97,7 @@ describe('Payment', () => {
         merchant: 'data'
       });
     });
+
     // then
     it('should send AUTH request with wallet and additional data', () => {
       instance.processPayment(
@@ -120,7 +120,9 @@ describe('Payment', () => {
         merchant: 'data',
         extra: 'some value'
       });
-    }); // then
+    });
+
+    // then
     it('should send CACHETOKENISE request with wallet and additional data', () => {
       instance.processPayment(
         ['CACHETOKENISE'],
@@ -143,14 +145,35 @@ describe('Payment', () => {
         extra: 'some value'
       });
     });
+
+    // then
+    it('should return response', () => {
+      // @ts-ignore
+      instance._stTransport.sendRequest = jest.fn().mockReturnValueOnce(
+        Promise.resolve({
+          response: {}
+        })
+      );
+      // @ts-ignore;
+      instance.processPayment().then(result => {
+        expect(result).toStrictEqual({
+          response: {}
+        });
+      });
+    });
   });
 
   // given
-  describe('threeDInitRequest', () => {
+  describe('threeDInitRequest()', () => {
+    // when
+    beforeEach(() => {
+      // @ts-ignore
+    });
+
     // then
     it('should send JSINIT request', () => {
       // @ts-ignore
-      instance._stTransport.sendRequest = jest.fn().mockReturnValue(
+      instance._stTransport.sendRequest = jest.fn().mockReturnValueOnce(
         Promise.resolve({
           cachetoken: 'content'
         })
@@ -163,14 +186,32 @@ describe('Payment', () => {
     });
 
     // then
-    it('should return resolved value', async () => {
+    it('should return result', () => {
       // @ts-ignore
-      await expect(instance.threeDInitRequest).toEqual(instance._cardinalCommerceCacheToken);
+      instance._stTransport.sendRequest = jest.fn().mockReturnValueOnce(
+        Promise.resolve({
+          jwt: 'somejwt',
+          response: {}
+        })
+      );
+      // @ts-ignore;
+      instance.threeDInitRequest().then(result => {
+        expect(result).toStrictEqual({
+          jwt: 'somejwt',
+          response: {}
+        });
+      });
     });
   });
 
   // given
-  describe('threeDQueryRequest', () => {
+  describe('threeDQueryRequest()', () => {
+    // when
+    beforeEach(() => {
+      // @ts-ignore
+      instance._stTransport.sendRequest = jest.fn();
+    });
+
     // then
     it('should send THREEDQUERY request', () => {
       // @ts-ignore
@@ -190,12 +231,29 @@ describe('Payment', () => {
       });
     });
   });
+
+  // given
+  describe('walletVerify()', () => {
+    // then
+    it('should send WALLETVERIFY request with walletverify', () => {
+      instance.walletVerify(walletverify);
+      // @ts-ignore
+      expect(instance._stTransport.sendRequest).toHaveBeenCalledWith({
+        requesttypedescriptions: ['WALLETVERIFY'],
+        walletsource: 'APPLEPAY',
+        walletmerchantid: '123456789',
+        walletvalidationurl: 'https://example.com',
+        walletrequestdomain: 'https://example2.com'
+      });
+    });
+  });
 });
 
 function paymentFixture() {
   const jwt =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0ZXN0X2p3dF9pc3N1ZXIiLCJwYXlsb2FkIjp7InNpdGVyZWZlcmVuY2UiOiJleGFtcGxlMTIzNDUiLCJiYXNlYW1vdW50IjoiMTAwMCIsImN1cnJlbmN5aXNvM2EiOiJHQlAifSwiaWF0IjoxNTE2MjM5MDIyfQ.jPuLMHxK3fznVddzkRoYC94hgheBXI1Y7zHAr7qNCig';
   let instance: Payment;
+  const cachetoken = 'somecachetoken';
   instance = new Payment(jwt, 'https://example.com');
   const card = {
     expirydate: '10/22',
@@ -212,5 +270,5 @@ function paymentFixture() {
     walletvalidationurl: 'https://example.com',
     walletrequestdomain: 'https://example2.com'
   };
-  return { card, wallet, walletverify, instance, jwt };
+  return { card, wallet, walletverify, instance, jwt, cachetoken };
 }
