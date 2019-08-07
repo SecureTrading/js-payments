fdce42be07e77d42789c0be2825ccae901942a30
Grzegorz Martowski <gmartowski@pgs-soft.com>
2019-08-07 13:29:42 +0200

    ST-296 Handle response JWT implementation - fix for linter

--- src/core/shared/Payment.ts
+++ src/core/shared/Payment.ts
@@ -65,7 +65,6 @@ export default class Payment {
    */
   public threeDInitRequest() {
     return this._stTransport.sendRequest(this._threeDInitRequestBody).then((result: { jwt: string; response: any }) => {
-      console.log(result);
       const {
         payload: { jwt, response }
       } = new StJwt(result.jwt);
