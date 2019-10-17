a1628d04ade03af8a76a1e3ecdea7d9c80652591
James Simpson <james.simpson@securetrading.com>
2019-10-17 12:22:09 +0100

    Animated card paths changed

--- src/components/animated-card/animated-card-logos.ts
+++ src/components/animated-card/animated-card-logos.ts
@@ -1,15 +1,15 @@
 const cardsLogos: { [brandName: string]: string } = {
-  amex: '../../../images/amex.png',
-  applePay: '../../../images/apple-pay.png',
-  astropaycard: '../../../images/astropay.png',
-  default: '../../../images/chip.png',
-  diners: '../../../images/diners.png',
-  discover: '../../../images/discover.png',
-  jcb: '../../../images/jcb.png',
-  maestro: '../../../images/maestro.png',
-  mastercard: '../../../images/mastercard.png',
-  piba: '../../../images/piba.png',
-  visa: '../../../images/visa.png'
+  amex: './images/amex.png',
+  applePay: './images/apple-pay.png',
+  astropaycard: './images/astropay.png',
+  default: './images/chip.png',
+  diners: './images/diners.png',
+  discover: './images/discover.png',
+  jcb: './images/jcb.png',
+  maestro: './images/maestro.png',
+  mastercard: './images/mastercard.png',
+  piba: './images/piba.png',
+  visa: './images/visa.png'
 };
 
 export { cardsLogos };
