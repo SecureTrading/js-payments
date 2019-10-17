0d2c9385856de794d0eb35d03ecd729ceecd1d32
Grzegorz Martowski <gmartowski@pgs-soft.com>
2019-10-17 16:26:33 +0200

    ST-352 Change the structure of loaded object and where to destructure object

--- src/ST.ts
+++ src/ST.ts
@@ -13,6 +13,7 @@ import GoogleAnalytics from './core/integrations/GoogleAnalytics';
 import VisaCheckout from './core/integrations/VisaCheckout';
 import VisaCheckoutMock from './core/integrations/VisaCheckoutMock';
 import {
+  IByPassInit,
   IComponentsConfig,
   IComponentsConfigSchema,
   IConfig,
@@ -249,24 +250,21 @@ class ST {
   private readonly _config: IConfig;
   private readonly _livestatus: number = 0;
   private readonly _submitCallback: any;
-  private readonly _threedinit: string;
+  private _threedinit: string;
   private commonFrames: CommonFrames;
 
   constructor(config: IConfig) {
     const { analytics, animatedCard, init, livestatus, submitCallback, translations } = config;
-    if (analytics) {
-      const ga = new GoogleAnalytics();
-    }
     if (init) {
-      const {
-        init: { cachetoken, threedinit }
-      } = config;
-      this._threedinit = threedinit;
+      const { cachetoken, threedinit } = init;
       this._cachetoken = cachetoken;
+      this._threedinit = threedinit;
     }
-    this._livestatus = livestatus;
     this._animatedCard = animatedCard;
+    this._livestatus = livestatus;
     this._submitCallback = submitCallback;
+
+    this._initGoogleAnalytics(analytics);
     this._config = ST._addDefaults(config);
     Utils.setLocalStorageItem(ST.TRANSLATION_STORAGE_NAME, translations);
     ST._validateConfig(this._config, IConfigSchema);
@@ -343,6 +341,18 @@ class ST {
       this._cachetoken,
       this._threedinit
     );
+  }
+
+  /**
+   * @param init
+   * @private
+   */
+  private _initGoogleAnalytics(init: boolean) {
+    if (init) {
+      const ga = new GoogleAnalytics();
+    } else {
+      return false;
+    }
   }
 
   /**
