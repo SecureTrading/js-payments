9b6d880cbd4da7f9fc4aa804e66acadf9020c48f
Grzegorz Martowski <gmartowski@pgs-soft.com>
2019-11-13 10:23:26 +0100

    ST-385 Couple of minor fixes after failing compilation

--- example/index.ts
+++ example/index.ts
@@ -3,11 +3,11 @@
  * This is code fired on merchant's side.
  * It can be treated as a reference for merchants how to integrate with STJS.
  */
-import './style.scss';
 import Card from '@securetrading/js-payments-card/dist/stcard.js';
 import '@securetrading/js-payments-card/dist/stcardstyle.css';
+import './style.scss';
 
 const card = new Card({
-  locale: 'en_GB',
-  animatedCardContainer: 'st-animated-card'
+  animatedCardContainer: 'st-animated-card',
+  locale: 'en_GB'
 });
