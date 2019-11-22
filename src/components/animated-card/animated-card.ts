d4850b96a4def6daa227a6a9c72cbd6d353437d6
Grzegorz Martowski <gmartowski@pgs-soft.com>
2019-11-22 16:31:47 +0100

    ST-384 Adjust js-payments-card library to communication with animated card

--- src/components/animated-card/animated-card.ts
+++ src/components/animated-card/animated-card.ts
@@ -1,7 +1,11 @@
 import '@securetrading/js-payments-card/dist/stcardstyle.css';
 import Card from '@securetrading/js-payments-card/stcard.js';
 
-const card = new Card({
-  animatedCardContainer: 'st-animated-card',
-  locale: 'en_GB'
-});
+if (Card) {
+  const card: Card = new Card({
+    animatedCardContainer: 'st-animated-card',
+    locale: 'en_GB'
+  });
+} else {
+  throw new Error('Animated card has not been loaded');
+}
