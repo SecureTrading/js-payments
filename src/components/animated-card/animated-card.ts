eacb7a9b6d0444b10993d34ff7310ce830c0dae3
Grzegorz Martowski <gmartowski@pgs-soft.com>
2019-12-04 15:13:34 +0100

    ST-384 Adjust callbacks to passing values from js-payments to js-payments-card

--- src/components/animated-card/animated-card.ts
+++ src/components/animated-card/animated-card.ts
@@ -4,28 +4,37 @@ import Card from '@securetrading/js-payments-card/stcard.js';
 import MessageBus from '../../core/shared/MessageBus';
 
 // @ts-ignore
-function setMessageBusSubscriber(event: string, func: () => void) {
-  const messageBus: MessageBus = new MessageBus();
-  messageBus.subscribe(event, (data: IFormFieldState) => {
-    console.error(event);
-    console.error(data);
-    func();
-  });
-}
-
-// @ts-ignore
 if (Card && document.URL.includes('animated')) {
   console.error('Jest karta 1');
   // @ts-ignore
   const card: Card = new Card({
-    locale: 'en_GB',
-    animatedCardContainer: 'st-animated-card'
+    animatedCardContainer: 'st-animated-card',
+    locale: 'en_GB'
   });
 
-  setMessageBusSubscriber(MessageBus.EVENTS.CHANGE_CARD_NUMBER, card.onCardNumberChange());
-  setMessageBusSubscriber(MessageBus.EVENTS.CHANGE_EXPIRATION_DATE, card.onExpirationDateChange());
-  setMessageBusSubscriber(MessageBus.EVENTS.CHANGE_SECURITY_CODE, card.onSecurityCodeChange());
-  setMessageBusSubscriber(MessageBus.EVENTS.FOCUS_CARD_NUMBER, card.onFieldFocus());
-  setMessageBusSubscriber(MessageBus.EVENTS.FOCUS_EXPIRATION_DATE, card.onFieldFocus());
-  setMessageBusSubscriber(MessageBus.EVENTS.FOCUS_SECURITY_CODE, card.onFieldFocus());
+  (function() {
+    console.error('setMessageBusSubscriber');
+    const messageBus: MessageBus = new MessageBus();
+    messageBus.subscribe(MessageBus.EVENTS.CHANGE_CARD_NUMBER, (data: IFormFieldState) => {
+      const { value } = data;
+      console.error(data);
+      card.onCardNumberChange(value, true);
+    });
+    messageBus.subscribe(MessageBus.EVENTS.CHANGE_EXPIRATION_DATE, (data: IFormFieldState) => {
+      const { value } = data;
+      console.error(data);
+      card.onExpirationDateChange(value, true);
+    });
+    messageBus.subscribe(MessageBus.EVENTS.CHANGE_SECURITY_CODE, (data: IFormFieldState) => {
+      const { value } = data;
+      console.error(data);
+      card.onSecurityCodeChange(value, true);
+    });
+  })();
+
+  // setMessageBusSubscriber(MessageBus.EVENTS.CHANGE_EXPIRATION_DATE, card.onExpirationDateChange());
+  // setMessageBusSubscriber(MessageBus.EVENTS.CHANGE_SECURITY_CODE, card.onSecurityCodeChange());
+  // setMessageBusSubscriber(MessageBus.EVENTS.FOCUS_CARD_NUMBER, card.onFieldFocus());
+  // setMessageBusSubscriber(MessageBus.EVENTS.FOCUS_EXPIRATION_DATE, card.onFieldFocus());
+  // setMessageBusSubscriber(MessageBus.EVENTS.FOCUS_SECURITY_CODE, card.onFieldFocus());
 }
