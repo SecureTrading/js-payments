b678f6e00b857a48d0fd51327f68d4c49289e3f1
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-01-15 14:59:34 +0100

    ST-348 Restore some deleted typedocs

--- src/core/classes/StCodec.class.ts
+++ src/core/classes/StCodec.class.ts
@@ -13,7 +13,7 @@ interface IStRequest {
   expirydate?: string;
   pan?: string;
   securitycode?: string;
-  termurl?: string;
+  termurl?: string; // TODO shouldn't be needed for CC request but this needs to wait for 153 release
 }
 
 class StCodec {
@@ -34,6 +34,13 @@ class StCodec {
   public static jwt: string;
   public static originalJwt: string;
 
+  /**
+   * Generate a unique ID for a request
+   * (this is informational. it doesn't need to be cryptographically random since one of those is allocated server-side)
+   * @param length The total length of the Request ID
+   *   (since we prepend 'J-' the random section will be 2 char shorter)
+   * @return A newly generated random request ID
+   */
   public static _createRequestId(length = 10) {
     return (
       'J-' +
@@ -61,6 +68,12 @@ class StCodec {
     return responseContent;
   }
 
+  /** Publishes translated response as a TRANSACTION_COMPLETE event
+   * to allow the page to submit to the merchant server
+   * @param responseData The decoded response from the gateway
+   * @param jwtResponse The raw JWT response from the gateway
+   * @param threedresponse the response from Cardinal commerce after call to ACS
+   */
   public static publishResponse(responseData: IResponseData, jwtResponse?: string, threedresponse?: string) {
     const translator = new Translator(StCodec._locale);
     responseData.errormessage = translator.translate(responseData.errormessage);
