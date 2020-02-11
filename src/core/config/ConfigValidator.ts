9351883f8253045d7f162b58a65c187a377eaabe
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2020-02-11 16:29:58 +0100

    removed console.log

--- src/core/config/ConfigValidator.ts
+++ src/core/config/ConfigValidator.ts
@@ -7,7 +7,6 @@ import { ConfigSchema } from './schema/ConfigSchema';
 @Service()
 export class ConfigValidator {
   validate(config: IConfig): ValidationError | null {
-    console.log(config);
     return Joi.validate(config, ConfigSchema).error;
   }
 }
