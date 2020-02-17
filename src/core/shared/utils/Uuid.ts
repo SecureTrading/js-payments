202729ea3633e89c34d82dcc0f35d8b3e33058ed
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2020-02-17 16:24:12 +0100

    changed the visibility of uuid methods

--- src/core/shared/utils/Uuid.ts
+++ src/core/shared/utils/Uuid.ts
@@ -3,13 +3,13 @@ export class Uuid {
     return window.crypto ? Uuid.uuidv4crypto() : Uuid.uuidv4random();
   }
 
-  public static uuidv4crypto(): string {
+  private static uuidv4crypto(): string {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, (c: string) =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   }
 
-  public static uuidv4random(): string {
+  private static uuidv4random(): string {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c: string) => {
       const r = Math.random() * 16 | 0;
       const v = c === 'x' ? r : (r & 0x3 | 0x8);
