f4effac80b4e91509914d60b408642328a065677
James Simpson <james.simpson@securetrading.com>
2020-02-28 09:06:25 +0000

    Only set the cachetoken value for cardinal if it isn't already set

--- src/core/shared/Payment.ts
+++ src/core/shared/Payment.ts
@@ -55,8 +55,12 @@ export class Payment {
         payload: { jwt, response }
       } = new StJwt(result.jwt);
       const threeDInitResult = { jwt, response: response[0] };
-      // @ts-ignore
-      this._cardinalCommerceCacheToken = result.response.cachetoken;
+      // We should always use the id from the original cachetoken to link up with the THREEDQUERY
+      // We've already passed this into Cardinal and they have used it for the fingerprint by this point
+      if (this._cardinalCommerceCacheToken === undefined) {
+        // @ts-ignore
+        this._cardinalCommerceCacheToken = result.response.cachetoken;
+      }
       return threeDInitResult;
     });
   }
