318aca5c010e1c426e6fd4792fc305d65d14965a
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-03-04 11:13:05 +0100

    ST-545 Let merchant specify not including iframes

--- src/components/notification-frame/notification-frame.ts
+++ src/components/notification-frame/notification-frame.ts
@@ -1,7 +1,6 @@
 import './notification-frame.scss';
 import { NotificationFrame } from './NotificationFrame';
 import { Container } from 'typedi';
-import { Notification } from '../../core/shared/Notification';
 import { FramesHub } from '../../core/services/message-bus/FramesHub';
 import { Selectors } from '../../core/shared/Selectors';
 
@@ -9,6 +8,6 @@ import { Selectors } from '../../core/shared/Selectors';
   Container.get(FramesHub)
     .waitForFrame(Selectors.CONTROL_FRAME_IFRAME)
     .subscribe(() => {
-      return NotificationFrame.ifFieldExists() && Container.get(Notification);
+      return NotificationFrame.ifFieldExists() && Container.get(NotificationFrame);
     });
 })();
