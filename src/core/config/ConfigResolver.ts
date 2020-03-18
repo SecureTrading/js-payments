f0d73e186f3c3c2f4f585a05c898ad802a6dfdb6
James Simpson <james.simpson@securetrading.com>
2020-03-18 20:18:56 +0000

    Change default for notifications to be true to keep backwards compatibility

--- src/core/config/ConfigResolver.ts
+++ src/core/config/ConfigResolver.ts
@@ -65,7 +65,8 @@ export class ConfigResolver {
       init: this._isTruthy(config.init) ? config.init : { cachetoken: '', threedinit: '' },
       jwt: this._isTruthy(config.jwt) ? config.jwt : '',
       livestatus: this._isTruthy(config.livestatus) ? config.livestatus : 0,
-      notifications: this._isTruthy(config.notifications) ? config.notifications : false,
+      // Can't use isTruthy for notifications because we default to true so undefined wants to act as if it's truthy
+      notifications: config.notifications !== undefined ? config.notifications : true,
       origin: this._isTruthy(config.origin) ? config.origin : window.location.origin,
       panIcon: this._isTruthy(config.panIcon) ? config.panIcon : false,
       placeholders: this._isTruthy(config.placeholders) ? config.placeholders : { pan: '', expirydate: '', securitycode: '' },
