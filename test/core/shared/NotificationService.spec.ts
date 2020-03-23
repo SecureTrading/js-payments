349df563fec90fb1b8da44f8168b1a0c76ece115
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-03-23 14:19:52 +0100

    ST-550 Fixing unit tests after changes in Notifications (splitting on Notification and NotificationService)

--- test/core/shared/NotificationService.spec.ts
+++ test/core/shared/NotificationService.spec.ts
@@ -1,20 +1,25 @@
 import { NotificationService } from '../../../src/core/services/notification/NotificationService';
 import { MessageBus } from '../../../src/core/shared/MessageBus';
 import { ConfigProvider } from '../../../src/core/config/ConfigProvider';
-import { Container } from 'typedi';
 import { NotificationType } from '../../../src/core/models/constants/NotificationType';
+import { instance, mock, verify, when } from 'ts-mockito';
 
 // given
 describe('NotificationService', () => {
+
+  // given
+  const messageBus: MessageBus = mock(MessageBus);
+  const configProvider: ConfigProvider = mock(ConfigProvider);
+  const notificationService: NotificationService = new NotificationService(
+    instance(messageBus), instance(configProvider)
+  );
   // given
   describe('error function has been called', () => {
-
-    const instance: NotificationService = Container.get(NotificationService);
 
     // when
     beforeEach(() => {
       // @ts-ignore
-      instance._configProvider.getConfig = jest.fn().mockReturnValueOnce({
+      when(configProvider.getConfig()).thenReturn({
         disableNotification: false,
         submitOnError: false,
         submitOnSuccess: false
@@ -22,10 +27,58 @@ describe('NotificationService', () => {
     });
 
     // then
-    it.skip('should call _setNotification with error message and error type of notification', () => {
-      instance.error('Test value');
+    it('should call _setNotification with error message and error type of notification', () => {
+      notificationService.error('Test value');
+      verify(messageBus.publish({
+        data: { type: NotificationType.Error, content: 'Test value' },
+        type: MessageBus.EVENTS_PUBLIC.NOTIFICATION
+      }, true)).once();
+    });
+  });
+
+  // given
+  describe('success function has been called', () => {
+
+    // when
+    beforeEach(() => {
       // @ts-ignore
-      expect(instance._setNotification).toHaveBeenCalledWith(NotificationType.Error, 'Test value');
+      when(configProvider.getConfig()).thenReturn({
+        disableNotification: false,
+        submitOnError: false,
+        submitOnSuccess: false
+      });
+    });
+
+    // then
+    it('should call _setNotification with success message and error type of notification', () => {
+      notificationService.error('Test value');
+      verify(messageBus.publish({
+        data: { type: NotificationType.Success, content: 'Test value' },
+        type: MessageBus.EVENTS_PUBLIC.NOTIFICATION
+      }, true)).once();
+    });
+  });
+
+  // given
+  describe('info function has been called', () => {
+
+    // when
+    beforeEach(() => {
+      // @ts-ignore
+      when(configProvider.getConfig()).thenReturn({
+        disableNotification: false,
+        submitOnError: false,
+        submitOnSuccess: false
+      });
+    });
+
+    // then
+    it('should call _setNotification with info message and error type of notification', () => {
+      notificationService.error('Test value');
+      verify(messageBus.publish({
+        data: { type: NotificationType.Info, content: 'Test value' },
+        type: MessageBus.EVENTS_PUBLIC.NOTIFICATION
+      }, true)).once();
     });
   });
 });
