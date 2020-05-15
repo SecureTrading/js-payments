a5cc14e8338a336ff2c734835a3a6f6298ce6ed6
James Simpson <james.simpson@securetrading.com>
2020-05-15 21:41:21 +0100

    Add BRANCH_NAME env variable

--- .github/workflows/scripts/set_branch_name.py
+++ .github/workflows/scripts/set_branch_name.py
@@ -2,6 +2,6 @@ import os
 
 
 print(os.environ)
-branch_name = os.environ["GITHUB_BRANCH"]
+branch_name = os.environ["GITHUB_REF"]
 print(branch_name)
 os.environ["BRANCH_NAME"] = "-".join(branch_name.split("/")[2:])
