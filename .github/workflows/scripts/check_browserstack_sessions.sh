3a82fe12d748139a29236b70398cd9bb52b7cd5f
James Simpson <james.simpson@securetrading.com>
2020-05-17 12:59:01 +0100

    Change browserstack check to check again after a backoff period

--- .github/workflows/scripts/check_browserstack_sessions.sh
+++ .github/workflows/scripts/check_browserstack_sessions.sh
@@ -5,7 +5,7 @@ SLEEP=10
 ATTEMPT=1
 BACKOFF=20 # Maximum time to wait to see if browserstack is still available
 
-checkSessions() {
+check_sessions() {
   echo "Checking if browserstack has active sessions"
   SESSION_DATA=$(curl -su "${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}" "https://api.browserstack.com/automate/plan.json")
   echo $SESSON_DATA
@@ -15,25 +15,25 @@ checkSessions() {
   echo "Active sessions ${ACTIVE_SESSIONS}/${MAX_ACTIVE_SESSIONS} queued: ${QUEUED_SESSIONS}"
   if [ $ACTIVE_SESSIONS -lt $MAX_ACTIVE_SESSIONS ] && [ $QUEUED_SESSIONS -lt 1 ]
   then
-    return 1;
+    return 1
   else
-    return 0;
+    return 0
   fi
 }
 
 SESSION_AVAILABLE=false
 while :
 do
-  checkSessions
+  check_sessions
   AVAILABLE=$?
-  if [ $AVAILABLE ]
+  if [ $AVAILABLE -eq 1 ]
   then
     WAIT_TO_CHECK_AGAIN=$(( (RANDOM % BACKOFF ) + 1 ))
     echo "WAITING ${WAIT_TO_CHECK_AGAIN} to see if its still the case"
     sleep $WAIT_TO_CHECK_AGAIN
-    checkSessions
+    check_sessions
     AVAILABLE=$?
-    if [ $AVAILABLE ]
+    if [ $AVAILABLE -eq 1 ]
     then
       SESSION_AVAILABLE=true
       break
