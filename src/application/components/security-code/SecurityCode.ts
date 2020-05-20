96853e894496be93bbfbb6225eeb44f8f555d9e4
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-05-20 11:37:40 +0200

    STJS-617 Parenttrasactionreference issue - AMEX card is not recognizable due to lack of maskedpan in Observable

--- src/application/components/security-code/SecurityCode.ts
+++ src/application/components/security-code/SecurityCode.ts
@@ -8,13 +8,15 @@ import { Selectors } from '../../core/shared/Selectors';
 import { Validation } from '../../core/shared/Validation';
 import { Service } from 'typedi';
 import { ConfigProvider } from '../../core/services/ConfigProvider';
-import { map, tap } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 import { ofType } from '../../../shared/services/message-bus/operators/ofType';
 import { IFormFieldState } from '../../core/models/IFormFieldState';
 import { merge, Observable } from 'rxjs';
 import JwtDecode from 'jwt-decode';
 import { IDecodedJwt } from '../../core/models/IDecodedJwt';
 import { iinLookup } from '@securetrading/ts-iin-lookup';
+import { IThreeDInitResponse } from '../../core/models/IThreeDInitResponse';
+import { BrowserSessionStorage } from '../../../shared/services/storage/BrowserSessionStorage';
 
 @Service()
 export class SecurityCode extends FormField {
@@ -37,7 +39,11 @@ export class SecurityCode extends FormField {
   private _securityCodeLength$: Observable<number>;
   private _validation: Validation;
 
-  constructor(private _configProvider: ConfigProvider, private _messageBus: MessageBus) {
+  constructor(
+    private _configProvider: ConfigProvider,
+    private _messageBus: MessageBus,
+    private _sessionStorage: BrowserSessionStorage
+  ) {
     super(Selectors.SECURITY_CODE_INPUT, Selectors.SECURITY_CODE_MESSAGE, Selectors.SECURITY_CODE_LABEL);
     this._formatter = new Formatter();
     this._validation = new Validation();
@@ -72,13 +78,11 @@ export class SecurityCode extends FormField {
       map(jwt => JwtDecode<IDecodedJwt>(jwt).payload.pan)
     );
 
-    const maskedPanFromJsInit$: Observable<string> = this._messageBus.pipe(
-      ofType(MessageBus.EVENTS_PUBLIC.JSINIT_RESPONSE),
-      map((event: IMessageBusEvent) => event.data.maskedpan)
-    );
+    const maskedPanFromJsInit$: Observable<string> = this._sessionStorage.select(store => store['app.maskedpan']);
 
     return merge(cardNumberInput$, cardNumberFromJwt$, maskedPanFromJsInit$).pipe(
-      map(cardNumber => (cardNumber ? iinLookup.lookup(cardNumber).cvcLength[0] : 3))
+      filter(Boolean),
+      map((cardNumber: string) => (cardNumber ? iinLookup.lookup(cardNumber).cvcLength[0] : 3))
     );
   }
 
@@ -172,7 +176,6 @@ export class SecurityCode extends FormField {
 
   private _subscribeSecurityCodeChange(): void {
     this._messageBus.subscribe(MessageBus.EVENTS.CHANGE_SECURITY_CODE_LENGTH, (length: number) => {
-      console.error(length, 'CVV length');
       this._checkSecurityCodeLength(length);
       this._getPlaceholder(length);
       this.placeholder = this._configProvider.getConfig().placeholders.securitycode || this._getPlaceholder(length);
