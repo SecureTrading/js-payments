1c7567f1efd8e4a8e9f2e7c7cfa602396bbdf8b2
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-06-03 09:28:26 +0200

    Visa checkout changes in properties of bvutton

--- src/application/core/models/visa-checkout/IVisaButtonSettings.ts
+++ src/application/core/models/visa-checkout/IVisaButtonSettings.ts
@@ -1,7 +1,7 @@
 export interface IVisaButtonSettings {
   size?: number;
-  height: 34 | 47 | 94;
-  width: number;
+  height?: 34 | 47 | 94;
+  width?: number;
   locale?: string;
   color?: 'neutral' | 'standard';
   cardBrands?: 'VISA' | 'MASTERCARD' | 'AMEX' | 'DISCOVER' | 'ELECTRON' | 'ELO';
