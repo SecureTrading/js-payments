2ea03ce267b68cbd7c4c3b385a630d3c2ebd241c
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2020-06-16 16:32:05 +0200

    added sharing cybertonica mock's tid between frames

--- src/testing/mocks/CybertonicaMock.ts
+++ src/testing/mocks/CybertonicaMock.ts
@@ -1,20 +1,27 @@
 import { ICybertonica } from '../../application/core/integrations/ICybertonica';
 import { Service } from 'typedi';
+import { BrowserLocalStorage } from '../../shared/services/storage/BrowserLocalStorage';
 
 @Service()
 export class CybertonicaMock implements ICybertonica {
+  private static TID_KEY: string = 'app.tid';
   private static readonly MOCKED_TID = '63d1d099-d635-41b6-bb82-96017f7da6bb';
-  private tid: Promise<string> = Promise.resolve(undefined);
+
+  constructor(private storage: BrowserLocalStorage) {}
 
   init(apiUserName: string): Promise<string> {
-    if (apiUserName) {
-      this.tid = Promise.resolve(CybertonicaMock.MOCKED_TID);
+    if (!apiUserName) {
+      return Promise.resolve(undefined);
     }
 
-    return this.tid;
+    const tid = CybertonicaMock.MOCKED_TID;
+
+    this.storage.setItem(CybertonicaMock.TID_KEY, tid);
+
+    return Promise.resolve(tid);
   }
 
   getTransactionId(): Promise<string> {
-    return this.tid;
+    return Promise.resolve(this.storage.getItem(CybertonicaMock.TID_KEY));
   }
 }
