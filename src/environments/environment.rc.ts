de6c5b32881ec1cd04e5a25eade23885c47d913e
Grzegorz Martowski <gmartowski@pgs-soft.com>
2020-08-05 13:31:37 +0200

    STJS-801 Ability to create release candidate

--- src/environments/environment.rc.ts
+++ src/environments/environment.rc.ts
@@ -1,19 +1,11 @@
 declare const WEBSERVICES_URL: string;
-
 const GATEWAY_URL: string = 'https://webservices.securetrading.net/jwt/';
 const MOCK_GATEWAY_URL: string = 'https://merchant.example.com:8443';
 
 export const environment = {
-  APM_NAMES: {
-    APPLE_PAY: 'APPLEPAY',
-    VISA_CHECKOUT: 'VISACHECKOUT'
-  },
-  APPLE_PAY_URLS: {
-    MOCK_DATA_URL: 'https://merchant.example.com:8443/applePaymentStatus'
-  },
   CARDINAL_COMMERCE: {
     CONFIG: {
-      logging: { level: 'on' }
+      logging: { level: 'off' }
     },
     MOCK: {
       AUTHENTICATE_CARD_URL: `${MOCK_GATEWAY_URL}/cardinalAuthenticateCard`
@@ -21,28 +13,23 @@ export const environment = {
     SONGBIRD_LIVE_URL: 'https://songbird.cardinalcommerce.com/edge/v1/songbird.js',
     SONGBIRD_TEST_URL: 'https://songbirdstag.cardinalcommerce.com/edge/v1/songbird.js'
   },
-  CARDINAL_COMMERCE_CONFIG: {
-    logging: { level: 'on' }
-  },
-  // @ts-ignore
   FRAME_URL: WEBSERVICES_URL,
   GATEWAY_URL,
-  GA_MEASUREMENT_ID: '42057093-5',
-  GA_SCRIPT_SRC: 'https://www.google-analytics.com/analytics_debug.js',
+  GA_MEASUREMENT_ID: '42057093-6',
+  GA_SCRIPT_SRC: 'https://www.google-analytics.com/analytics.js',
   NOTIFICATION_TTL: 7000,
   VISA_CHECKOUT_URLS: {
     LIVE_BUTTON_URL: 'https://secure.checkout.visa.com/wallet-services-web/xo/button.png',
     LIVE_SDK: 'https://secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js',
-    MOCK_DATA_URL: `${MOCK_GATEWAY_URL}/visaPaymentStatus`,
     TEST_BUTTON_URL: 'https://sandbox.secure.checkout.visa.com/wallet-services-web/xo/button.png',
     TEST_SDK: 'https://sandbox-assets.secure.checkout.visa.com/checkout-widget/resources/js/integration/v1/sdk.js'
   },
   CYBERTONICA: {
     CYBERTONICA_LIVE_URL: 'https://cyber.securetrading.net/js/v2/afeasdwqwdasd.js'
   },
-  production: false,
+  production: true,
   testEnvironment: false,
   overrideDomain: '',
-  SENTRY_DSN: null as string,
+  SENTRY_DSN: 'https://6319b9ff1fb14ba48cd2c9025d67bd2d@o402164.ingest.sentry.io/5262818',
   SENTRY_WHITELIST_URLS: ['https://webservices.securetrading.net']
 };
