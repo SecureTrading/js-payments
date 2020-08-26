d82abbb3e97206bb5f230da582fc4e0321c399d4
Maciej Krawczyk <mackrawczyk@pgs-soft.com>
2020-08-26 10:21:30 +0200

    changed the event on which the listener is unbound

--- src/client/message-subscribers/PreventNavigationPopup.ts
+++ src/client/message-subscribers/PreventNavigationPopup.ts
@@ -1,8 +1,7 @@
 import { IMessageSubscriber } from '../../shared/services/message-bus/interfaces/IMessageSubscriber';
 import { Inject, Service } from 'typedi';
 import { ofType } from '../../shared/services/message-bus/operators/ofType';
-import { filter, takeUntil } from 'rxjs/operators';
-import { IMessageBusEvent } from '../../application/core/models/IMessageBusEvent';
+import { takeUntil } from 'rxjs/operators';
 import { MessageSubscriberToken, WINDOW } from '../../shared/dependency-injection/InjectionTokens';
 import { MessageBus } from '../../application/core/shared/message-bus/MessageBus';
 import { PUBLIC_EVENTS } from '../../application/core/models/constants/EventTypes';
@@ -23,11 +22,7 @@ export class PreventNavigationPopup implements IMessageSubscriber {
       .subscribe(() => this.window.addEventListener('beforeunload', beforeUnloadHandler));
 
     messageBus
-      .pipe(
-        ofType(PUBLIC_EVENTS.TRANSACTION_COMPLETE),
-        filter((event: IMessageBusEvent) => !event.data.acsurl), // we ignore 3dquery responses
-        takeUntil(destroy)
-      )
+      .pipe(ofType(PUBLIC_EVENTS.CALL_MERCHANT_SUBMIT_CALLBACK), takeUntil(destroy))
       .subscribe(() => this.window.removeEventListener('beforeunload', beforeUnloadHandler));
   }
 }
